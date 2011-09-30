aab680d1a2f84a8aaf232c168b8f14ff66974519
Julien Biezemans <jb@jbpros.com>
2011-09-30 22:56:41 +0200

    Update web example to use cucumber-html formatter

--- example/example.js
+++ example/example.js
@@ -1,32 +1,114 @@
-function runFeature() {
-  var Cucumber = require('./cucumber');
-  var supportCode;
-  var output          = $('#output');
-  var errors          = $('#errors');
-  var errorsContainer = $('#errors-container');
-  var featureSource   = $('#feature').val();
-  eval('supportCode   = function() {' + $('#step-definitions').val() + '};');
-  var cucumber        = Cucumber(featureSource, supportCode);
-  var listener  = Cucumber.Listener.ProgressFormatter({logToConsole: false, logToFunction: function(message) {
-    output.val(output.val() + message);
-  }});
-  cucumber.attachListener(listener);
-  output.val('');
-  errors.text('');
-  errorsContainer.hide();
-  try {
-    cucumber.start(function() {});
-  } catch(err) {
-    errorsContainer.show();
-    var errMessage = err.message || err;
-    var buffer = (errors.text() == '' ? errMessage : errors.text() + "\n\n" + errMessage);
-    errors.text(buffer);
-    throw err;
-  };
-};
+(function($) {
+  var CucumberHTMLListener = function($root) {
+    var CucumberHTML = require('cucumber-html');
+    var formatter    = new CucumberHTML.DOMFormatter($root);
 
-$(function() {
-  Gherkin = { Lexer: function() { return Lexer; } };
-  $('#run-feature').click(runFeature);
-  $('#errors-container').hide();
-});
+    formatter.uri('report.feature');
+
+    var currentStep;
+
+    var self = {
+      hear: function hear(event, callback) {
+        var eventName = event.getName();
+        switch (eventName) {
+        case 'BeforeFeature':
+          var feature = event.getPayloadItem('feature');
+          formatter.feature({
+            keyword     : feature.getKeyword(),
+            name        : feature.getName(),
+            line        : feature.getLine(),
+            description : feature.getDescription()
+          });
+          break;
+
+        case 'BeforeScenario':
+          var scenario = event.getPayloadItem('scenario');
+          formatter.scenario({
+            keyword     : scenario.getKeyword(),
+            name        : scenario.getName(),
+            line        : scenario.getLine(),
+            description : scenario.getDescription()
+          });
+          break;
+
+        case 'BeforeStep':
+          var step = event.getPayloadItem('step');
+          self.handleAnyStep(step);
+          break;
+
+        case 'StepResult':
+          var result;
+          var stepResult = event.getPayloadItem('stepResult');
+          if (stepResult.isSuccessful()) {
+            result = {status: 'passed'};
+          } else if (stepResult.isPending()) {
+            result = {status: 'pending'};
+          } else {
+            var error = stepResult.getFailureException();
+            var errorMessage = error.stack || error;
+            result = {status: 'failed', error_message: errorMessage};
+          }
+          formatter.match({uri:'report.feature', step: {line: currentStep.getLine()}});
+          formatter.result(result);
+          break;
+
+        case 'UndefinedStep':
+        case 'SkippedStep':
+          var step = event.getPayloadItem('step');
+          self.handleAnyStep(step);
+          formatter.match({uri:'report.feature', step: {line: step.getLine()}});
+          formatter.result({status:'skipped'});
+          break;
+
+        default:
+          if (console && console.log)
+            console.log('event not handled: '+eventName);
+        }
+        callback();
+      },
+
+      handleAnyStep: function handleAnyStep(step) {
+        formatter.step({
+          keyword     : step.getKeyword(),
+          name        : step.getName(),
+          line        : step.getLine(),
+        });
+        currentStep = step;
+      }
+    };
+    return self;
+  };
+
+  function runFeature() {
+    var Cucumber        = require('./cucumber');
+    var supportCode;
+    var output          = $('#output');
+    var errors          = $('#errors');
+    var errorsContainer = $('#errors-container');
+    var featureSource   = $('#feature').val();
+    eval('supportCode   = function() {' + $('#step-definitions').val() + '};');
+    var cucumber        = Cucumber(featureSource, supportCode);
+    var $output         = $('#output');
+    $output.empty();
+    var listener        = CucumberHTMLListener($output);
+    cucumber.attachListener(listener);
+
+    errors.text('');
+    errorsContainer.hide();
+    try {
+      cucumber.start(function() {});
+    } catch(err) {
+      errorsContainer.show();
+      var errMessage = err.message || err;
+      var buffer = (errors.text() == '' ? errMessage : errors.text() + "\n\n" + errMessage);
+      errors.text(buffer);
+      throw err;
+    };
+  };
+
+  $(function() {
+    Gherkin = { Lexer: function() { return Lexer; } };
+    $('#run-feature').click(runFeature);
+    $('#errors-container').hide();
+  });
+})(jQuery);
--- example/server.js
+++ example/server.js
@@ -3,8 +3,9 @@ var server = connect.createServer();
 
 server.use(connect.static(__dirname));
 server.use(require('browserify')({
-  require: {'cucumber':           './lib/cucumber.js',
-            './gherkin/lexer/en': 'gherkin/lib/gherkin/lexer/en'},
+  require: ['cucumber-html',
+            {'cucumber': './lib/cucumber.js'},
+            {'./gherkin/lexer/en': 'gherkin/lib/gherkin/lexer/en'}],
   ignore: ['./cucumber/cli', 'connect']
 }));
 var port = process.env.PORT || 9797;
