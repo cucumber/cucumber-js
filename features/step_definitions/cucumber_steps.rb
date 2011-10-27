7fa7f17b4dc09b4c56cc4312e5611bc150f492cf
Tristan Dunn <tristanzdunn@gmail.com>
2011-10-26 21:38:30 -0500

    Feeble attempt at the somewhat convoluted process to test hooks.

--- features/step_definitions/cucumber_steps.rb
+++ features/step_definitions/cucumber_steps.rb
@@ -17,3 +17,32 @@ end
 Then /^the mapping is run$/ do
   assert_passed "a mapping"
 end
+
+Given /^a passing before hook$/ do
+  append_support_code <<-EOF
+this.Before(function(callback) {
+  this.calls = ["Before"];
+  callback();
+});
+EOF
+end
+
+Given /^a passing after hook$/ do
+  append_support_code <<-EOF
+this.After(function(callback) {
+  this.calls.push("After");
+  console.log(this.calls.join(","));
+  callback();
+});
+EOF
+end
+
+When /^Cucumber executes a scenario$/ do
+  append_step_definition("Cucumber executes a step definition", "this.calls.push('Step');\ncallback();")
+  scenario_with_steps "A scenario", "Given Cucumber executes a step definition\n"
+  run_feature
+end
+
+Then /^the (after|before) hook is fired (?:after|before) the scenario$/ do |type|
+  assert_partial_output("Before,Step,After", all_output)
+end
