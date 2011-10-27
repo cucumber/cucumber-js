689d8260642997c42a58205c8c334d1551678403
Julien Biezemans <jb@jbpros.com>
2011-10-27 23:31:21 +0200

    Clean up JS step definitions
    - Remove awful "prepare()" calls that were used to reset state between scenarios
    - Use a World object to handle states
    - Minor encapsulation improvements

--- features/step_definitions/cucumber_steps.js
+++ features/step_definitions/cucumber_steps.js
@@ -1,230 +1,115 @@
 var cucumberSteps = function() {
   var Given = When = Then = this.defineStep;
 
-  var shouldPrepare = true;
-  var featureSource;
-  var stepDefinitions;
-  var touchedSteps;
-  var lastRunSucceeded;
-  var lastRunOutput;
+  this.World = require('./cucumber_world');
 
   Given(/^a scenario "([^"]*)" with:$/, function(scenarioName, steps, callback) {
-    prepare();
-    featureSource += "Feature: A feature\n";
-    featureSource += "  Scenario: " + scenarioName + "\n";
-    featureSource += steps.replace(/^/gm, '    ');
+    this.featureSource += "Feature: A feature\n";
+    this.featureSource += "  Scenario: " + scenarioName + "\n";
+    this.featureSource += steps.replace(/^/gm, '    ');
     callback();
   });
 
   Given(/^the step "([^"]*)" has a passing mapping$/, function(stepName, callback) {
-    prepare();
-    stepDefinitions += "Given(/^" + stepName + "$/, function(callback) {\
-  touchStep(\"" + stepName + "\");\
+    this.stepDefinitions += "Given(/^" + stepName + "$/, function(callback) {\
+  world.touchStep(\"" + stepName + "\");\
   callback();\
 });\n";
     callback();
   });
 
   Given(/^the step "([^"]*)" has a failing mapping$/, function(stepName, callback) {
-    prepare();
-    stepDefinitions += "Given(/^" + stepName + "$/, function(callback) {\
-  touchStep(\"" + stepName + "\");\
+    this.stepDefinitions += "Given(/^" + stepName + "$/, function(callback) {\
+  world.touchStep(\"" + stepName + "\");\
   throw(new Error('I was supposed to fail.'));\
 });\n";
     callback();
   });
 
   Given(/^the step "([^"]*)" has a mapping failing with the message "([^"]*)"$/, function(stepName, message, callback) {
-    prepare();
-    stepDefinitions += "Given(/^" + stepName + "$/, function(callback) {\
-  touchStep(\"" + stepName + "\");\
+    this.stepDefinitions += "Given(/^" + stepName + "$/, function(callback) {\
+  world.touchStep(\"" + stepName + "\");\
   throw(new Error('" + message + "'));\
 });\n";
     callback();
   });
 
   Given(/^the step "([^"]*)" has a pending mapping$/, function(stepName, callback) {
-    prepare();
-    stepDefinitions += "Given(/^" + stepName + "$/, function(callback) {\
-  touchStep(\"" + stepName + "\");\
+    this.stepDefinitions += "Given(/^" + stepName + "$/, function(callback) {\
+  world.touchStep(\"" + stepName + "\");\
   callback.pending('I am pending.');\
 });\n";
     callback();
   });
 
   Given(/^the following feature:$/, function(feature, callback) {
-    prepare();
-    featureSource = feature;
+    this.featureSource = feature;
     callback();
   });
 
   When(/^Cucumber executes the scenario "([^"]*)"$/, function(scenarioName, callback) {
-    runFeature(callback);
+    this.runFeature(callback);
   });
 
   When(/^Cucumber runs the feature$/, function(callback) {
-    runFeature(callback);
+    this.runFeature(callback);
   });
 
   When(/^Cucumber runs the scenario with steps for a calculator$/, function(callback) {
     RpnCalculator = require('../support/rpn_calculator');
-    supportCode = function() { require('./calculator_steps').initialize.call(this, RpnCalculator) };
-    runFeatureWithSupportCodeSource(supportCode, callback);
+    var supportCode = function() { require('./calculator_steps').initialize.call(this, RpnCalculator) };
+    this.runFeatureWithSupportCodeSource(supportCode, callback);
   });
 
   Then(/^the scenario passes$/, function(callback) {
-    if (!lastRunSucceeded)
-      throw(new Error("Expected the scenario to pass but it failed"));
+    this.assertPassedScenario();
     callback();
   });
 
   Then(/^the scenario fails$/, function(callback) {
-    assertFailedScenario();
+    this.assertFailedScenario();
     callback();
   });
 
   Then(/^the scenario is pending$/, function(callback) {
-    assertPendingScenario();
+    this.assertPendingScenario();
     callback();
   });
 
   Then(/^the scenario is undefined$/, function(callback) {
-    assertUndefinedScenario();
+    this.assertUndefinedScenario();
     callback();
   });
 
   Then(/^the scenario called "([^"]*)" is reported as failing$/, function(scenarioName, callback) {
-    assertScenarioReportedAsFailing(scenarioName);
+    this.assertScenarioReportedAsFailing(scenarioName);
     callback();
   });
 
   Then(/^the scenario called "([^"]*)" is not reported as failing$/, function(scenarioName, callback) {
-    assertScenarioNotReportedAsFailing(scenarioName);
+    this.assertScenarioNotReportedAsFailing(scenarioName);
     callback();
   });
 
   Then(/^the step "([^"]*)" passes$/, function(stepName, callback) {
-    assertPassedStep(stepName);
+    this.assertPassedStep(stepName);
     callback();
   });
 
 
   Then(/^the step "([^"]*)" is skipped$/, function(stepName, callback) {
-    assertSkippedStep(stepName);
+    this.assertSkippedStep(stepName);
     callback();
   });
 
   Then(/^the feature passes$/, function(callback) {
-    assertPassingFeature();
+    this.assertPassedFeature();
     callback();
   });
 
   Then(/^the failure message "([^"]*)" is output$/, function(message, callback) {
-    assertFailureMessage(message);
+    this.assertFailureMessage(message);
     callback();
   });
-
-  function prepare() {
-    if (shouldPrepare) {
-      shouldPrepare = false;
-      touchedSteps    = [];
-      featureSource   = "";
-      stepDefinitions = "";
-    }
-  }
-
-  function runFeature(callback) {
-    var supportCode;
-    var supportCodeSource = "supportCode = function() {\n  var Given = When = Then = this.defineStep;\n" +
-      stepDefinitions + "};\n";
-    eval(supportCodeSource);
-    runFeatureWithSupportCodeSource(supportCode, callback);
-  }
-
-  function runFeatureWithSupportCodeSource(supportCode, callback) {
-    var Cucumber          = require('../../lib/cucumber');
-    var cucumber          = Cucumber(featureSource, supportCode);
-    var formatter         = Cucumber.Listener.ProgressFormatter({logToConsole: false});
-    cucumber.attachListener(formatter);
-    cucumber.start(function(succeeded) {
-      lastRunSucceeded = succeeded;
-      lastRunOutput    = formatter.getLogs();
-      Cucumber.Debug.notice(lastRunOutput, 'cucumber output', 5);
-      shouldPrepare = true;
-      callback();
-    });
-  }
-
-  function touchStep(string) {
-    touchedSteps.push(string);
-  }
-
-  function isStepTouched(pattern) {
-    return (touchedSteps.indexOf(pattern) >= 0);
-  }
-
-  function assertPassingFeature() {
-    assertNoPartialOutput("failed", lastRunOutput);
-    assertSuccess();
-  }
-
-  function assertFailedScenario() {
-    assertPartialOutput("1 scenario (1 failed)", lastRunOutput);
-    assertFailure();
-  }
-
-  function assertPendingScenario() {
-    assertPartialOutput("1 scenario (1 pending)", lastRunOutput);
-    assertSuccess();
-  }
-
-  function assertUndefinedScenario() {
-    assertPartialOutput("1 scenario (1 undefined)", lastRunOutput);
-    assertSuccess();
-  }
-
-  function assertScenarioReportedAsFailing(scenarioName) {
-    assertPartialOutput("# Scenario: " + scenarioName, lastRunOutput);
-    assertFailure();
-  }
-
-  function assertScenarioNotReportedAsFailing(scenarioName) {
-    assertNoPartialOutput("# Scenario: " + scenarioName, lastRunOutput);
-  }
-
-  function assertPassedStep(stepName) {
-    if (!isStepTouched(stepName))
-      throw(new Error("Expected step \"" + stepName + "\" to have passed."));
-  }
-
-  function assertSkippedStep(stepName) {
-    if (isStepTouched(stepName))
-      throw(new Error("Expected step \"" + stepName + "\" to have been skipped."));
-  }
-
-  function assertSuccess() {
-    if (!lastRunSucceeded)
-      throw(new Error("Expected Cucumber to succeed but it failed."));
-  }
-
-  function assertFailure() {
-    if (lastRunSucceeded)
-      throw(new Error("Expected Cucumber to fail but it succeeded."));
-  }
-
-  function assertFailureMessage(message) {
-    assertPartialOutput(message, lastRunOutput);
-    assertFailure();
-  }
-
-  function assertPartialOutput(expected, actual) {
-    if (actual.indexOf(expected) < 0)
-      throw(new Error("Expected:\n\"" + actual + "\"\nto match:\n\"" + expected + "\""));
-  }
-
-  function assertNoPartialOutput(expected, actual) {
-    if (actual.indexOf(expected) >= 0)
-      throw(new Error("Expected:\n\"" + actual + "\"\nnot to match:\n\"" + expected + "\""));
-  }
 };
 module.exports = cucumberSteps;
---
+++ features/step_definitions/cucumber_world.js
@@ -0,0 +1,111 @@
+var CucumberWorld = function() {
+  this.touchedSteps    = [];
+  this.featureSource   = "";
+  this.stepDefinitions = "";
+  this.runOutput       = "";
+  this.runSucceeded    = false;
+};
+
+var proto = CucumberWorld.prototype;
+
+proto.runFeature = function runFeature(callback) {
+  var supportCode;
+  var supportCodeSource = "supportCode = function() {\n  var Given = When = Then = this.defineStep;\n" +
+    this.stepDefinitions + "};\n";
+  var world = this;
+  eval(supportCodeSource);
+  this.runFeatureWithSupportCodeSource(supportCode, callback);
+}
+
+proto.runFeatureWithSupportCodeSource = function runFeatureWithSupportCodeSource(supportCode, callback) {
+  var world     = this;
+  var Cucumber  = require('../../lib/cucumber');
+  var cucumber  = Cucumber(this.featureSource, supportCode);
+  var formatter = Cucumber.Listener.ProgressFormatter({logToConsole: false});
+  cucumber.attachListener(formatter);
+  cucumber.start(function(succeeded) {
+    world.runSucceeded = succeeded;
+    world.runOutput    = formatter.getLogs();
+    Cucumber.Debug.notice(world.runOutput, 'cucumber output', 5);
+    callback();
+  });
+}
+
+proto.touchStep = function touchStep(string) {
+  this.touchedSteps.push(string);
+}
+
+proto.isStepTouched = function isStepTouched(pattern) {
+  return (this.touchedSteps.indexOf(pattern) >= 0);
+}
+
+proto.assertPassedFeature = function assertPassedFeature() {
+  this.assertNoPartialOutput("failed", this.runOutput);
+  this.assertSuccess();
+}
+
+proto.assertPassedScenario = function assertPassedScenario() {
+  this.assertPartialOutput("1 scenario (1 passed)", this.runOutput);
+  this.assertSuccess();
+}
+
+proto.assertFailedScenario = function assertFailedScenario() {
+  this.assertPartialOutput("1 scenario (1 failed)", this.runOutput);
+  this.assertFailure();
+}
+
+proto.assertPendingScenario = function assertPendingScenario() {
+  this.assertPartialOutput("1 scenario (1 pending)", this.runOutput);
+  this.assertSuccess();
+}
+
+proto.assertUndefinedScenario = function assertUndefinedScenario() {
+  this.assertPartialOutput("1 scenario (1 undefined)", this.runOutput);
+  this.assertSuccess();
+}
+
+proto.assertScenarioReportedAsFailing = function assertScenarioReportedAsFailing(scenarioName) {
+  this.assertPartialOutput("# Scenario: " + scenarioName, this.runOutput);
+  this.assertFailure();
+}
+
+proto.assertScenarioNotReportedAsFailing = function assertScenarioNotReportedAsFailing(scenarioName) {
+  this.assertNoPartialOutput("# Scenario: " + scenarioName, this.runOutput);
+}
+
+proto.assertPassedStep = function assertPassedStep(stepName) {
+  if (!this.isStepTouched(stepName))
+    throw(new Error("Expected step \"" + stepName + "\" to have passed."));
+}
+
+proto.assertSkippedStep = function assertSkippedStep(stepName) {
+  if (this.isStepTouched(stepName))
+    throw(new Error("Expected step \"" + stepName + "\" to have been skipped."));
+}
+
+proto.assertSuccess = function assertSuccess() {
+  if (!this.runSucceeded)
+    throw(new Error("Expected Cucumber to succeed but it failed."));
+}
+
+proto.assertFailure = function assertFailure() {
+  if (this.runSucceeded)
+    throw(new Error("Expected Cucumber to fail but it succeeded."));
+}
+
+proto.assertFailureMessage = function assertFailureMessage(message) {
+  this.assertPartialOutput(message, this.runOutput);
+  this.assertFailure();
+}
+
+proto.assertPartialOutput = function assertPartialOutput(expected, actual) {
+  if (actual.indexOf(expected) < 0)
+    throw(new Error("Expected:\n\"" + actual + "\"\nto match:\n\"" + expected + "\""));
+}
+
+proto.assertNoPartialOutput = function assertNoPartialOutput(expected, actual) {
+  if (actual.indexOf(expected) >= 0)
+    throw(new Error("Expected:\n\"" + actual + "\"\nnot to match:\n\"" + expected + "\""));
+}
+
+module.exports = CucumberWorld;
