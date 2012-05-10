fa5aa85c041c1e4ed269904151559388aba83278
Julien Biezemans <jb@jbpros.com>
2012-05-10 22:20:42 +0200

    Fix example app static path

--- example/server.js
+++ example/server.js
@@ -1,9 +1,16 @@
 var connect    = require('connect');
 var server     = connect.createServer();
 var port       = process.env.PORT || 9797;
-var static_dir = __dirname.slice(0, __dirname.lastIndexOf("/"));
 
-server.use(connect.static(static_dir));
+var staticDirs = [
+  __dirname,
+  __dirname.slice(0, __dirname.lastIndexOf("/"))
+];
+
+staticDirs.forEach(function (dir) {
+  server.use(connect.static(dir));
+});
+
 server.listen(port);
 
-console.log('Accepting connections on port ' + port + ' from ' + static_dir + '...');
+console.log('Accepting connections on port ' + port + ' from ' + staticDirs.join(', ') + '...');
