02577cd5189634bc30ebefe862e6933d667ae7cd
Aaron Garvey <agarvey@3vr.com>
2012-06-22 13:32:20 -0700

    Replace findit with walkdir (close #73)
    This fixes step definition and support code loading on Windows.

--- spec/cucumber/cli/argument_parser/path_expander_spec.js
+++ spec/cucumber/cli/argument_parser/path_expander_spec.js
@@ -93,19 +93,19 @@ describe("Cucumber.Cli.ArgumentParser.PathExpander", function() {
   });
 
   describe("expandDirectoryWithRegexp()", function() {
-    var findit = require('findit');
+    var walkdir = require('walkdir');
     var directory, regexp, innerPaths;
 
     beforeEach(function() {
       directory  = createSpy("directory");
       regexp     = createSpyWithStubs("regexp", {test: null});
       innerPaths = [createSpy("inner path 1"), createSpy("inner path 2"), createSpy("inner path 3")];
-      spyOn(findit, 'sync').andReturn(innerPaths);
+      spyOn(walkdir, 'sync').andReturn(innerPaths);
     });
 
     it("recursively finds the directory inner paths", function() {
       PathExpander.expandDirectoryWithRegexp(directory, regexp);
-      expect(findit.sync).toHaveBeenCalledWith(directory);
+      expect(walkdir.sync).toHaveBeenCalledWith(directory);
     });
 
     it("tests the regexp against each inner path", function() {
