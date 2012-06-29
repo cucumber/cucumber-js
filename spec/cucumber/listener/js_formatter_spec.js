ae288733d34e13a9fa5fee86c41b0673a7094293
Dan McAulay <dmcaulay@gmail.com>
2012-06-28 20:55:54 -0700

    more testing around js formatter

--- spec/cucumber/listener/js_formatter_spec.js
+++ spec/cucumber/listener/js_formatter_spec.js
@@ -22,15 +22,105 @@ describe("Cucumber.Listener.JSFormatter", function () {
     });
   });
 
-  describe("handleStepResultEvent()", function () {
-    var event, callback, stepResult;
+  describe("handleBeforeFeatureEvent()", function () {
+    var event, callback, feature, jsFeature;
 
     beforeEach(function () {
-      step = createSpy();
-      stepResult = createSpyWithStubs("step result", {getStep: step);
+      jsFeature = createSpy('js feature');
+      feature = createSpy("feature");
+      event      = createSpyWithStubs("event", {getPayloadItem: feature});
+      callback   = createSpy("Callback");
+      spyOn(jsFormatter, 'buildFeature').andReturn(jsFeature);
+    });
+
+    it("gets the feature from the event payload", function () {
+      jsFormatter.handleBeforeFeatureEvent(event, callback);
+      expect(event.getPayloadItem).toHaveBeenCalledWith('feature');
+    });
+
+    it("builds the JSON feature", function () {
+      jsFormatter.handleBeforeFeatureEvent(event, callback);
+      expect(jsFormatter.buildFeature).toHaveBeenCalledWith(feature);
+    });
+
+    it("assigns the new feature to the current feature", function () {
+      jsFormatter.handleBeforeFeatureEvent(event, callback);
+      expect(jsFormatter.currentFeature).toEqual(jsFeature);
+    });
+
+    it("calls back", function () {
+      jsFormatter.handleBeforeFeatureEvent(event, callback);
+      expect(callback).toHaveBeenCalled();
+    });
+  });
+
+  describe("handleAfterFeatureEvent()", function () {
+    var callback;
+
+    beforeEach(function () {
+      callback   = createSpy("Callback");
+      jsFormatter.currentFeature = {test:'hello'};
+    });
+
+    it("logs the JSON feature", function() {
+      jsFormatter.handleAfterFeatureEvent(null, callback);
+      expect(jsFormatter.log).toHaveBeenCalledWith('{"test":"hello"}');
+    });
+
+    it("calls back", function () {
+      jsFormatter.handleAfterFeatureEvent(event, callback);
+      expect(callback).toHaveBeenCalled();
+    });
+  });
+
+  describe("handleBeforeScenarioEvent()", function () {
+    var event, callback, scenario, jsScenario;
+
+    beforeEach(function () {
+      jsScenario = createSpy('js scenario');
+      scenario = createSpy("scenario");
+      event      = createSpyWithStubs("event", {getPayloadItem: scenario});
+      callback   = createSpy("Callback");
+      spyOn(jsFormatter, 'buildScenario').andReturn(jsScenario);
+      spyOn(jsFormatter, 'addScenario');
+    });
+
+    it("gets the scenario from the event payload", function () {
+      jsFormatter.handleBeforeScenarioEvent(event, callback);
+      expect(event.getPayloadItem).toHaveBeenCalledWith('scenario');
+    });
+
+    it("builds the JSON scenario", function () {
+      jsFormatter.handleBeforeScenarioEvent(event, callback);
+      expect(jsFormatter.buildScenario).toHaveBeenCalledWith(scenario);
+    });
+
+    it("assigns the new feature to the current scenario", function () {
+      jsFormatter.handleBeforeScenarioEvent(event, callback);
+      expect(jsFormatter.currentScenario).toEqual(jsScenario);
+    });
+
+    it("adds the new scenario to the result", function () {
+      jsFormatter.handleBeforeScenarioEvent(event, callback);
+      expect(jsFormatter.addScenario).toHaveBeenCalledWith(jsScenario);
+    });
+
+    it("calls back", function () {
+      jsFormatter.handleBeforeScenarioEvent(event, callback);
+      expect(callback).toHaveBeenCalled();
+    });
+  });
+
+  describe("handleStepResultEvent()", function () {
+    var event, callback, stepResult, step, jsStep;
+
+    beforeEach(function () {
+      jsStep = createSpy('js step');
+      step = createSpy('step');
+      stepResult = createSpyWithStubs("step result", {getStep: step});
       event      = createSpyWithStubs("event", {getPayloadItem: stepResult});
       callback   = createSpy("Callback");
-      spyOn(jsFormatter, 'buildStep');
+      spyOn(jsFormatter, 'buildStep').andReturn(jsStep);
       spyOn(jsFormatter, 'addStep');
     });
 
@@ -50,14 +140,12 @@ describe("Cucumber.Listener.JSFormatter", function () {
     });
 
     it("adds the new step to the result", function () {
-      newStep = createSpy();
-      jsFormatter.buildStep.andReturn(newStep);
       jsFormatter.handleStepResultEvent(event, callback);
-      expect(jsFormatter.addStem).toHaveBeenCalledWith(newStep);
+      expect(jsFormatter.addStep).toHaveBeenCalledWith(jsStep);
     });
 
     it("calls back", function () {
-      progressFormatter.handleStepResultEvent(event, callback);
+      jsFormatter.handleStepResultEvent(event, callback);
       expect(callback).toHaveBeenCalled();
     });
   });
