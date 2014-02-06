084da1ae94257bc1f844aaec7435a7b3ea32cd3e
Ben Van Treese <vantreeseba@gmail.com>
2014-02-06 14:36:57 -0600

    re-adding scenario outline feature, cleaning up some steps, cleanin up scenario outline and data table

--- features/step_definitions/cucumber_steps.js
+++ features/step_definitions/cucumber_steps.js
@@ -21,18 +21,6 @@ var cucumberSteps = function() {
       this.addAroundHook(callback);
   });
 
-  Given(/^an untagged hook$/, function(callback) {
-    this.addUntaggedHook(callback);
-  });
-
-  Given(/^a hook tagged with "([^"]*)"$/, function(tags, callback) {
-    this.addHookWithTags(tags, callback);
-  });
-
-  Given(/^an around hook tagged with "([^"]*)"$/, function(tags, callback) {
-    this.addAroundHookWithTags(tags, callback);
-  });
-
   Given(/^the step "([^"]*)" has a failing mapping$/, function(stepName, callback) {
     this.addFailingMapping(stepName, {}, callback);
   });
@@ -119,31 +107,6 @@ setTimeout(callback.pending, 10);\
     callback();
   });
 
-  Given(/^a scenario without any tags$/, function(callback) {
-    this.addPassingScenarioWithoutTags();
-    callback();
-  });
-
-  Given(/^a scenario tagged with "([^"]*)"$/, function(tag, callback) {
-    this.addPassingScenarioWithTags(tag);
-    callback();
-  });
-
-  Given(/^a scenario tagged with "([^"]*)" and "([^"]*)"$/, function(tag1, tag2, callback) {
-    this.addPassingScenarioWithTags(tag1, tag2);
-    callback();
-  });
-
-  Given(/^a scenario tagged with "([^"]*)", "([^"]*)" and "([^"]*)"$/, function(tag1, tag2, tag3, callback) {
-    this.addPassingScenarioWithTags(tag1, tag2, tag3);
-    callback();
-  });
-
-  Given(/^a feature tagged with "([^"]*)"$/, function(tag, callback) {
-    this.createEmptyFeature({tags: [tag]});
-    callback();
-  });
-
   Given(/^several features$/, function(callback) {
     this.features = [
       ["feature1", "Feature: One\n\n  Scenario:\n"],
@@ -157,10 +120,6 @@ setTimeout(callback.pending, 10);\
     this.runFeature({}, callback);
   });
 
-  When(/^Cucumber executes a scenario(?: with no tags)?$/, function(callback) {
-    this.runAScenario(callback);
-  });
-
   this.When(/^Cucumber executes a scenario using that mapping$/, function(callback) {
     this.runAScenarioCallingMapping(callback);
   });
@@ -171,11 +130,6 @@ setTimeout(callback.pending, 10);\
 
   When(/^Cucumber executes a scenario that calls a function on the explicit World object$/, function(callback) {
     this.runAScenarioCallingWorldFunction(callback);
-  });
-
-  When(/^Cucumber executes a scenario tagged with "([^"]*)"$/, function(tag, callback) {
-    this.addPassingScenarioWithTags(tag);
-    this.runFeature({}, callback);
   });
 
   When(/^Cucumber runs the feature$/, function(callback) {
@@ -207,38 +161,6 @@ callback();\
 });\n";
     this.runFeature({}, callback);
   });
-
-  When(/^Cucumber executes scenarios tagged with "([^"]*)"$/, function(tag, callback) {
-    this.runFeature({tags: [tag]}, callback);
-  });
-
-  When(/^Cucumber executes scenarios not tagged with "([^"]*)"$/, function(tag, callback) {
-    this.runFeature({tags: ['~'+tag]}, callback);
-  });
-
-  When(/^Cucumber executes scenarios tagged with "([^"]*)" or "([^"]*)"$/, function(tag1, tag2, callback) {
-    this.runFeature({tags: [tag1 + ', ' + tag2]}, callback);
-  });
-
-  When(/^Cucumber executes scenarios tagged with both "([^"]*)" and "([^"]*)"$/, function(tag1, tag2, callback) {
-    this.runFeature({tags: [tag1, tag2]}, callback);
-  });
-
-  When(/^Cucumber executes scenarios not tagged with "([^"]*)" nor "([^"]*)"$/, function(tag1, tag2, callback) {
-    this.runFeature({tags: ['~'+tag1, '~'+tag2]}, callback);
-  });
-
-  When(/^Cucumber executes scenarios not tagged with both "([^"]*)" and "([^"]*)"$/, function(tag1, tag2, callback) {
-    this.runFeature({tags: ['~' + tag1 + ', ~' + tag2]}, callback);
-  });
-
-  When(/^Cucumber executes scenarios tagged with "([^"]*)" or without "([^"]*)"$/, function(tag1, tag2, callback) {
-    this.runFeature({tags: [tag1 + ', ~' + tag2]}, callback);
-  });
-
-  When(/^Cucumber executes scenarios tagged with "([^"]*)" but not with both "([^"]*)" and "([^"]*)"$/, function(tag1, tag2, tag3, callback) {
-    this.runFeature({tags: [tag1, '~' + tag2, '~' + tag3]}, callback);
-});
 
   Then(/^the scenario passes$/, function(callback) {
     this.assertPassedScenario();
@@ -352,26 +274,6 @@ callback();\
 
   Then(/^the hook is not fired$/, function(callback) {
     this.assertCycleSequenceExcluding('hook');
-    callback();
-  });
-
-  Then(/^(?:only the first|the) scenario is executed$/, function(callback) {
-    this.assertExecutedNumberedScenarios(1);
-    callback();
-  });
-
-  Then(/^only the first two scenarios are executed$/, function(callback) {
-    this.assertExecutedNumberedScenarios(1, 2);
-    callback();
-  });
-
-  Then(/^only the third scenario is executed$/, function(callback) {
-    this.assertExecutedNumberedScenarios(3);
-    callback();
-  });
-
-  Then(/^only the second, third and fourth scenarios are executed$/, function(callback) {
-    this.assertExecutedNumberedScenarios(2, 3, 4);
     callback();
   });
 };
