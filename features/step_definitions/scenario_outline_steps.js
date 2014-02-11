d8efad682a183be0e561b2038365abb7047a106d
Ben Van Treese <vantreeseba@gmail.com>
2014-02-10 21:25:29 -0600

    cleaning up travis to remove trying to install bundler, actually implemented scenario outline steps, implemented snipper builder steps for example outline snippets

--- features/step_definitions/scenario_outline_steps.js
+++ features/step_definitions/scenario_outline_steps.js
@@ -3,13 +3,16 @@ var scenario_outline_steps = function() {
 	var World  = require('./cucumber_world').World;
 	this.World = World;
 
-	this.When(/^a ([^"]*) step$/, function (some, callback) {         
-	  callback();                                           
+	this.When(/^a (.*) step$/, function (some, callback) {
+	   this.assertTrue(some === "passing" || some === "failing");
+	   callback();
 	});                                                             
 	                                                                
-	this.Then(/^i get ([^"]*)$/, function (result, callback) {        
-	  callback();                                           
-	});                            
+	this.Then(/^i get (.*)$/, function (result, callback) {
+		this.assertTrue(result === "passed" || result === "skipped");
+	   callback();                                           
+	});            
+
 }
 
 module.exports = scenario_outline_steps;
