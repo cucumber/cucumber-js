ee7de77d728041f5a3460e59910df00699feecb0
jbergknoff <jonathan.bergknoff@makerstudios.com>
2014-03-15 17:26:02 +0000

    cleanup spec

--- spec/cucumber/ast/filter/modular_partitioning_spec.js
+++ spec/cucumber/ast/filter/modular_partitioning_spec.js
@@ -3,18 +3,16 @@ require('../../../support/spec_helper');
 describe("Cucumber.Ast.Filter.ModularPartitioning", function() {
   var Cucumber = requireLib('cucumber');
 
+  var numberPartitions = 4;
+  var remainder = 3;
+  var rule = Cucumber.Ast.Filter.ModularPartitioning(numberPartitions, remainder);
+
   describe("isSatisfiedByElement()", function() {
     it("should return true when element belongs to the requested partition", function() {
-      var numberPartitions = 4;
-      var remainder = 3;
-      var rule = Cucumber.Ast.Filter.ModularPartitioning(numberPartitions, remainder);
       expect(rule.isSatisfiedByElement({ "counter": 7 })).toBe(true);
     });
 
     it("should return false when element does not belong to the requested partition", function() {
-      var numberPartitions = 4;
-      var remainder = 3;
-      var rule = Cucumber.Ast.Filter.ModularPartitioning(numberPartitions, remainder);
       expect(rule.isSatisfiedByElement({ "counter": 6 })).toBe(false);
     });
   });
