87a86f7dfad83a6e6b7d965598865ffd251d85ff
Simon Lampen <simonlampen@vinsight.net>
2014-04-13 15:13:16 +1200

    Add command line argument handling for running single scenarios in feature files with the pattern featurefilename.feature:linenum

--- bin/cucumber.js
+++ bin/cucumber.js
@@ -3,23 +3,20 @@ var Cucumber = require('../lib/cucumber');
 var cli = Cucumber.Cli(process.argv);
 cli.run(function(succeeded) {
   var code = succeeded ? 0 : 1;
-  var exitFunction = function() {
-    process.exit(code);
-  };
 
-  // --- exit after waiting for all pending output ---
-  var waitingIO = false;
-  process.stdout.on('drain', function() {
-    if (waitingIO) {
-      // the kernel buffer is now empty
-      exitFunction();
-    }
+  process.on('exit', function() {
+    process.exit(code);
   });
-  if (process.stdout.write("")) {
-    // no buffer left, exit now:
-    exitFunction();
-  } else {
-    // write() returned false, kernel buffer is not empty yet...
-    waitingIO = true;
+
+  var timeoutId = setTimeout(function () {
+    console.error('Cucumber process timed out after waiting 60 seconds for the node.js event loop to empty.  There may be a resource leak.  Have all resources like database connections and network connections been closed properly?');
+    process.exit(code);    
+  }, 60 * 1000);
+
+  if (timeoutId.unref) {
+    timeoutId.unref();
+  }
+  else {
+    clearTimeout(timeoutId);
   }
 });
