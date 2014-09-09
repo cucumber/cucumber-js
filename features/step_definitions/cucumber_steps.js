fc132843e2e9e2243cc4390af178b1d467afd177
morrislaptop <craig.michael.morris@gmail.com>
2014-09-09 10:40:14 +0100

    Allow > 2 parameters in string step definitions

--- features/step_definitions/cucumber_steps.js
+++ features/step_definitions/cucumber_steps.js
@@ -71,6 +71,11 @@ setTimeout(callback.pending, 10);\
     callback();
   });
 
+  Given(/^a mapping with a string-based pattern and multiple parameters$/, function(callback) {
+    this.addStringBasedPatternMappingWithMultipleParameters();
+    callback();
+  });
+
   Given(/^the following feature:$/, function(feature, callback) {
     this.featureSource = feature;
     callback();
@@ -126,6 +131,10 @@ setTimeout(callback.pending, 10);\
 
   this.When(/^Cucumber executes a scenario that passes arguments to that mapping$/, function(callback) {
     this.runAScenarioCallingMappingWithParameters(callback);
+  });
+
+  this.When(/^Cucumber executes a scenario that passes multiple arguments to that mapping$/, function(callback) {
+    this.runAScenarioCallingMappingWithMultipleParameters(callback);
   });
 
   When(/^Cucumber executes a scenario that calls a function on the explicit World object$/, function(callback) {
@@ -209,6 +218,11 @@ callback();\
 
   Then(/^the mapping receives the arguments$/, function(callback) {
     this.assertPassedMappingWithArguments();
+    callback();
+  });
+
+  Then(/^the mapping receives the multiple arguments$/, function(callback) {
+    this.assertPassedMappingWithMultipleArguments();
     callback();
   });
 
