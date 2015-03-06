58bba1c6d676ecf74be0543814bd258ca1cda170
Julien Biezemans <jb@jbpros.com>
2015-03-06 08:11:38 +0100

    Separate source map from bundle
    This reduces the bundle size by ~50%.
    
    This also changes the way the bundler works. It doesn't return the bundle content through the callback anymore but writes it to a file directly. Same goes for the newly-generated source map file.

--- bundle-main.js
+++ bundle-main.js
@@ -1,2 +1,2 @@
-window.CucumberHTML = require('cucumber-html');
+global.CucumberHTML = require('cucumber-html');
 module.exports = require('./lib/cucumber');
--- bundler.js
+++ bundler.js
@@ -1,79 +1,73 @@
 var fs = require('fs');
 var through = require('through');
 var browserify = require('browserify');
+var exorcist = require('exorcist');
 
-function Bundler() {
+function Bundler(bundlePath) {
+  var mapPath = bundlePath + '.map';
   var source;
 
   var self = {
-    middleware: function (req, res, next) {
-      function serveBundle(res) {
-        console.log('/cucumber.js 200', source.length);
-        res.setHeader('Content-Type', 'application/javascript');
-        res.write(source);
-        res.end();
-      }
-
-      if (req.originalUrl === '/cucumber.js') {
-        if (!source) {
-          self.bundle(function (err) {
-            if (err) return next(err);
-            serveBundle(res);
-          });
-        } else {
-          serveBundle(res);
-        }
-      } else {
-        next();
-      }
-    },
-
     bundle: function (callback) {
-      var bundler = browserify({debug: true, standalone: 'Cucumber'});
-      bundler.transform({global: true}, function (file) {
-        var data = '';
+      var _callback = callback;
+      callback = function (err) {
+        if (_callback) _callback(err);
+        _callback = null;
+      };
 
-        function write (buf) { data += buf; }
+      var operation = browserify({debug: true, standalone: 'Cucumber'})
+          .transform({global: true}, fixGherkinLexers)
+          .transform({global:true}, 'uglifyify')
+          .exclude('./lib/cucumber/cli') // TODO: doesn't work, fix this
+          .require('./bundle-main', { expose: 'cucumber' })
+          .bundle()
+          .pipe(exorcist(mapPath))
+          .pipe(fs.createWriteStream(bundlePath, 'utf8'));
 
-        function end () {
-          var path = __dirname + '/node_modules/gherkin/lib';
-          var lexersPath = path + '/gherkin/lexer';
-          if (file === path + '/gherkin.js') {
-            // Patch gherkin so that all lexers are available statically:
-            var bufferPrefix = '';
-            var dirFiles = fs.readdirSync(lexersPath);
-            dirFiles.forEach(function (dirFile) {
-              var matches = dirFile.match(/^(.*)\.js$/);
-              if (matches && !dirFile.match(/\.min\.js$/)) {
-                bufferPrefix += 'require("./gherkin/lexer/' + matches[1] + '");\n';
-              }
-            });
-            data = bufferPrefix + data;
-          }
-          var ignoredFiles = [
-            __dirname + '/lib/cucumber/cli.js'
-          ];
-          if (ignoredFiles.indexOf(file) > -1) {
-            data = '';
-          }
-          this.queue(data);
-          this.queue(null);
-        }
-
-        return through(write, end);
+      operation.on('error', function (err) {
+        callback(err);
       });
-      bundler.transform({global:true}, 'uglifyify');
-      bundler.exclude('./lib/cucumber/cli'); // TODO: doesn't work, fix this
-      bundler.require('./bundle-main', { expose: 'cucumber' });
-      bundler.bundle(function (err, _source) {
-        if (err) return callback(err);
-        source = _source;
-        callback(null, source);
+
+      operation.on('finish', function () {
+        callback();
       });
     }
   };
 
   return self;
+}
+
+function fixGherkinLexers(file) {
+  var data = '';
+
+  function write (buf) { data += buf; }
+
+  function end () {
+    var path = __dirname + '/node_modules/gherkin/lib';
+    var lexersPath = path + '/gherkin/lexer';
+    if (file === path + '/gherkin.js') {
+      // Patch gherkin so that all lexers are available statically:
+      var bufferPrefix = '';
+      var dirFiles = fs.readdirSync(lexersPath);
+      dirFiles.forEach(function (dirFile) {
+        var matches = dirFile.match(/^(.*)\.js$/);
+        if (matches && !dirFile.match(/\.min\.js$/)) {
+          bufferPrefix += 'require("./gherkin/lexer/' + matches[1] + '");\n';
+        }
+      });
+      data = bufferPrefix + data;
+    }
+    var ignoredFiles = [
+      __dirname + '/lib/cucumber/cli.js'
+    ];
+    if (ignoredFiles.indexOf(file) > -1) {
+      data = '';
+    }
+    this.queue(data);
+    this.queue(null);
+  }
+
+  return through(write, end);
 }
 
 module.exports = Bundler;
