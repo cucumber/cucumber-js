23ff8608417b542c8c775c2ac5914e232b8c5dfa
Julien Biezemans <jb@jbpros.com>
2015-06-16 16:02:21 +0200

    Remove CLI resource leak timeout
    When run through the CLI, Cucumber was waiting for the event loop to
    empty before exiting and timing out after 60 seconds.
    
    This removes that behaviour: Cucumber is now exiting as soon as stdout is
    flushed. This means Cucumber is no longer responsible for leaked
    resources (e.g. unclosed network connections, etc.).

--- bin/cucumber.js
+++ bin/cucumber.js
@@ -4,19 +4,14 @@ var cli = Cucumber.Cli(process.argv);
 cli.run(function (succeeded) {
   var code = succeeded ? 0 : 1;
 
-  process.on('exit', function () {
+  function exitNow() {
     process.exit(code);
-  });
-
-  var timeoutId = setTimeout(function () {
-    console.error('Cucumber process timed out after waiting 60 seconds for the node.js event loop to empty.  There may be a resource leak.  Have all resources like database connections and network connections been closed properly?');
-    process.exit(code);
-  }, 60 * 1000);
-
-  if (timeoutId.unref) {
-    timeoutId.unref();
   }
-  else {
-    clearTimeout(timeoutId);
+
+  if (process.stdout.write('')) {
+    exitNow();
+  } else {
+    // write() returned false, kernel buffer is not empty yet...
+    process.stdout.on('drain', exitNow);
   }
 });
