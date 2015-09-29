d24cfee7082d66a212f1b180b41c95b85f805e87
charlierudolph <charles.rudolph@originate.com>
2015-09-28 22:26:45 -0700

    finish conversion

--- spec/cucumber/type/collection_spec.js
+++ spec/cucumber/type/collection_spec.js
@@ -1,175 +1,144 @@
-// require('../../support/spec_helper');
+require('../../support/spec_helper');
 
-// describe("Cucumber.Type.Collection", function () {
-//   var Cucumber = requireLib('cucumber');
-//   var collection, itemArray;
+describe("Cucumber.Type.Collection", function () {
+  var Cucumber = requireLib('cucumber');
+  var collection;
 
-//   beforeEach(function () {
-//     itemArray = [3, 1, 2];
-//     spyOn(itemArray, 'push');
-//     spyOn(itemArray, 'unshift');
-//     spyOn(itemArray, 'splice');
-//     spyOn(itemArray, 'indexOf');
-//     spyOn(global, 'Array').and.returnValue(itemArray);
-//     collection = Cucumber.Type.Collection();
-//   });
+  beforeEach(function () {
+    collection = Cucumber.Type.Collection();
+    collection.add('a');
+    collection.add('b');
+    collection.add('c');
+  });
 
-//   describe("constructor", function () {
-//     it("creates a new array to store the items in the collection", function () {
-//       expect(Array).toHaveBeenCalled();
-//     });
-//   });
+  describe("add()", function () {
+    it("pushes the item onto the end of the item array", function () {
+      collection.add('d');
+      expect(collection.toArray()).toEqual(['a','b','c','d']);
+    });
+  });
 
-//   describe("add()", function () {
-//     it("pushes the item onto the end of the item array", function () {
-//       var item = createSpy("collection item");
-//       collection.add(item);
-//       expect(itemArray.push).toHaveBeenCalledWith(item);
-//     });
-//   });
+  describe("insert()", function () {
+    it("inserts an item at a specific index in the item array", function () {
+      collection.insert(1, 'd');
+      expect(collection.toArray()).toEqual(['a','d','b','c']);
+    });
+  });
 
-//   describe("insert()", function () {
-//     it("inserts an item at a specific index in the item array", function () {
-//       var index = createSpy("index in the collection");
-//       var item = createSpy("collection item");
-//       collection.insert(index, item);
-//       expect(itemArray.splice).toHaveBeenCalledWith(index, 0, item);
-//     });
-//   });
+  describe("removeAtIndex()", function () {
+    it("removes an item at a specific index in the item array", function () {
+      collection.removeAtIndex(1);
+      expect(collection.toArray()).toEqual(['a','c']);
+    });
+  });
 
-//   describe("removeAtIndex()", function () {
-//     it("removes an item at a specific index in the item array", function () {
-//       var index = createSpy("index in the collection");
-//       collection.removeAtIndex(index);
-//       expect(itemArray.splice).toHaveBeenCalledWith(index, 1);
-//     });
-//   });
+  describe("unshift()", function () {
+    it("unshifts the item onto the start of the item array", function () {
+      collection.unshift('d');
+      expect(collection.toArray()).toEqual(['d','a','b','c']);
+    });
+  });
 
-//   describe("unshift()", function () {
-//     it("unshifts the item onto the start of the item array", function () {
-//       var item = createSpy("collection item");
-//       collection.unshift(item);
-//       expect(itemArray.unshift).toHaveBeenCalledWith(item);
-//     });
-//   });
+  describe("clear()", function () {
+    it("empties the item array", function () {
+      expect(collection.length()).toEqual(3);
+      collection.clear();
+      expect(collection.length()).toEqual(0);
+    });
+  });
 
-//   describe("clear()", function () {
-//     it("empties the item array", function () {
-//       expect(itemArray.length).toEqual(3);
-//       collection.clear();
-//       expect(itemArray.length).toEqual(0);
-//     });
-//   });
+  describe("indexOf()", function () {
+    it("gets the index of an item in the item array", function () {
+      expect(collection.indexOf('a')).toEqual(0);
+      expect(collection.indexOf('d')).toEqual(-1);
+    });
+  });
 
-//   describe("indexOf()", function () {
-//     it("gets the index of an item in the item array", function () {
-//       var item  = createSpy("collection item");
-//       var index = createSpy("index in the collection");
-//       itemArray.indexOf.and.returnValue(index);
-//       var actualIndex = collection.indexOf(item);
-//       expect(itemArray.indexOf).toHaveBeenCalledWith(item);
-//       expect(actualIndex).toBe(index);
-//     });
-//   });
+  describe("getAtIndex()", function () {
+    it("gets the item at a specific index in the item array", function () {
+      expect(collection.getAtIndex(0)).toEqual('a');
+      expect(collection.getAtIndex(1)).toEqual('b');
+      expect(collection.getAtIndex(2)).toEqual('c');
+    });
+  });
 
-//   describe("getAtIndex()", function () {
-//     it("gets the item at a specific index in the item array", function () {
-//       expect(collection.getAtIndex(0)).toEqual(3);
-//       expect(collection.getAtIndex(1)).toEqual(1);
-//       expect(collection.getAtIndex(2)).toEqual(2);
-//     });
-//   });
+  describe("getLast()", function () {
+    it("returns the latest added item from the array", function () {
+      expect(collection.getLast()).toEqual('c');
+    });
+  });
 
-//   describe("getLast()", function () {
-//     it("returns the latest added item from the array", function () {
-//       var lastItem = createSpy("last item");
-//       itemArray[itemArray.length] = lastItem;
-//       expect(collection.getLast()).toBe(lastItem);
-//     });
-//   });
+  describe("asyncForEach()", function () {
+    var userFunction, callback, itemCount;
+    var processedItems, allItemsProcessedBeforeCallback;
+    var delayItemProcessing;
 
-//   describe("asyncForEach()", function () {
-//     var userFunction, callback, itemCount;
-//     var processedItems, allItemsProcessedBeforeCallback;
-//     var delayItemProcessing;
+    beforeEach(function () {
+      processedItems = [];
+      allItemsProcessedBeforeCallback = false;
+      delayItemProcessing = false;
+      userFunction = createSpy("asyncForEach() user function").and.callFake(function (item, callback) {
+        processedItems.push(item);
+        if (!delayItemProcessing)
+          callback();
+      });
+      callback = createSpy("asyncForEach() callback").and.callFake(function () {
+        if (processedItems.length === itemCount)
+          allItemsProcessedBeforeCallback = true;
+      });
+      itemCount = collection.length();
+    });
 
-//     beforeEach(function () {
-//       processedItems = [];
-//       allItemsProcessedBeforeCallback = false;
-//       delayItemProcessing = false;
-//       userFunction = createSpy("asyncForEach() user function").and.callFake(function (item, callback) {
-//         processedItems.push(item);
-//         if (!delayItemProcessing)
-//           callback();
-//       });
-//       callback = createSpy("asyncForEach() callback").and.callFake(function () {
-//         if (processedItems.length === itemCount)
-//           allItemsProcessedBeforeCallback = true;
-//       });
-//       itemCount = itemArray.length;
-//     });
+    it("calls the user function on each item in the array with a callback", function () {
+      collection.asyncForEach(userFunction, callback);
+      var callIndex = 0;
+      expect(userFunction).toHaveBeenCalledTimes(3);
+      collection.toArray().forEach(function (item) {
+        var args = userFunction.calls.argsFor(callIndex++);
+        expect(args[0]).toBe(item);
+        expect(args[1]).toBeAFunction();
+      });
+    });
 
-//     it("calls the user function on each item in the array with a callback", function () {
-//       collection.asyncForEach(userFunction, callback);
-//       var callIndex = 0;
-//       expect(userFunction).toHaveBeenCalledTimes(3);
-//       itemArray.forEach(function (item) {
-//         var args = userFunction.calls[callIndex++].args;
-//         expect(args[0]).toBe(item);
-//         expect(args[1]).toBeAFunction ();
-//       });
-//     });
+    it("calls the asyncForEach() callback when all items have been processed and called their user function callback", function () {
+      collection.asyncForEach(userFunction, callback);
+      expect(callback).toHaveBeenCalled();
+      expect(allItemsProcessedBeforeCallback).toBeTruthy();
+    });
 
-//     it("calls the asyncForEach() callback when all items have been processed and called their user function callback", function () {
-//       collection.asyncForEach(userFunction, callback);
-//       expect(callback).toHaveBeenCalled();
-//       expect(allItemsProcessedBeforeCallback).toBeTruthy();
-//     });
+    it("does not call the asyncForEach() callback if not all items are processed", function () {
+      delayItemProcessing = true;
+      collection.asyncForEach(userFunction, callback);
+      expect(allItemsProcessedBeforeCallback).toBeFalsy();
+      expect(callback).not.toHaveBeenCalled();
+    });
 
-//     it("does not call the asyncForEach() callback if not all items are processed", function () {
-//       delayItemProcessing = true;
-//       collection.asyncForEach(userFunction, callback);
-//       expect(allItemsProcessedBeforeCallback).toBeFalsy();
-//       expect(callback).not.toHaveBeenCalled();
-//     });
+    it("does not process the next item until the current one is finished", function () {
+      delayItemProcessing = true;
+      collection.asyncForEach(userFunction, callback);
+      expect(userFunction).toHaveBeenCalledTimes(1);
+      var args = userFunction.calls.mostRecent().args;
+      expect(args[0]).toBe(collection.toArray()[0]);
+    });
+  });
 
-//     it("does not process the next item until the current one is finished", function () {
-//       delayItemProcessing = true;
-//       collection.asyncForEach(userFunction, callback);
-//       expect(userFunction).toHaveBeenCalledTimes(1);
-//       var args = userFunction.calls.mostRecent().args;
-//       expect(args[0]).toBe(itemArray[0]);
-//     });
+  describe("forEach()", function () {
+    it("calls forEach on a copy of the array", function () {
+      var items = [];
+      collection.forEach(function(item) {
+        items.push(item);
+      });
+      expect(items).toEqual(['a', 'b', 'c']);
+    });
+  });
 
-//     it("does not modify the original array", function () {
-//       var originalArray = itemArray.slice(0);
-//       collection.asyncForEach(userFunction, callback);
-//       expect(itemArray).toEqual(originalArray);
-//     });
-//   });
+  describe("sort()", function () {
+    it("sorts the array", function () {
+      var sorted = collection.sort(function(a, b) {
+        return b > a;
+      });
 
-//   describe("forEach()", function () {
-//     var userFunction = createSpy("userFunction");
-
-//     it("calls foreach on a copy of the array", function () {
-//       var itemsCopy = createSpy("items copy");
-//       spyOn(itemArray, 'slice').and.returnValue(itemsCopy);
-//       spyOnStub(itemsCopy, 'forEach');
-//       collection.forEach(userFunction);
-//       expect(itemArray.slice).toHaveBeenCalledWith(0);
-//       expect(itemsCopy.forEach).toHaveBeenCalledWith(userFunction);
-//     });
-//   });
-
-//   describe("sort()", function () {
-//     it("sorts the array", function () {
-//       var sorted = collection.sort(function(a, b) {
-//         return a > b;
-//       });
-
-//       expect(sorted.shift()).toEqual(1);
-//       expect(sorted.shift()).toEqual(2);
-//       expect(sorted.shift()).toEqual(3);
-//     });
-//   });
-// });
+      expect(sorted.toArray()).toEqual(['c', 'b', 'a']);
+    });
+  });
+});
