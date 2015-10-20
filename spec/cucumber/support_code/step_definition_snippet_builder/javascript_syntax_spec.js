52b03944a1b3be2ac5fd47b107f317ec73d3af28
charlierudolph <charles.rudolph@originate.com>
2015-10-20 17:28:07 -0600

    fix unit test

--- spec/cucumber/support_code/step_definition_snippet_builder/javascript_syntax_spec.js
+++ spec/cucumber/support_code/step_definition_snippet_builder/javascript_syntax_spec.js
@@ -6,9 +6,9 @@ describe('Cucumber.SupportCode.StepDefinitionSnippetBuilder.JavaScriptSyntax', f
 
   describe('build()', function () {
     it('returns the proper snippet', function () {
-      var actual = Syntax.build('functionName', '/^pattern$/', ['arg1', 'arg2', 'arg3'], 'comment');
+      var actual = Syntax.build('functionName', '/^pattern$/', ['arg1', 'arg2', 'callback'], 'comment');
       var expected =
-        'this.functionName(/^pattern$/, function (arg1, arg2, arg3) {' + '\n' +
+        'this.functionName(/^pattern$/, function (arg1, arg2, callback) {' + '\n' +
         '  // comment' + '\n' +
         '  callback.pending();' + '\n' +
         '});' + '\n';
