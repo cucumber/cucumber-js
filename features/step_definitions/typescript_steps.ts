a7eef4c3338d8f9b7564baa139b3e568992f3a7f
Damien Springuel <damien.springuel.1@gmail.com>
2017-03-01 16:52:04 -0600

    Deleted typescript specific feature

--- features/step_definitions/typescript_steps.ts
+++
@@ -1,7 +0,0 @@
-import {defineSupportCode, StepDefinitions} from '../../src/typings/cucumber';
-
-defineSupportCode(function(stepDef:StepDefinitions){
-    stepDef.Given("Given a typescript feature", () => {
-        console.log("Given works");
-    });
-});
