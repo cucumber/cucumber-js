231183a8a11c985ef7ced1155b7a75f5120a34b6
charlierudolph <charles.rudolph@originate.com>
2017-03-09 18:09:42 -0800

    export all formatters
    closes #781
    resolves #769

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ export {default as Cli} from './cli'
 export {default as FeatureParser} from './cli/feature_parser'
 export {default as Formatter} from './formatter'
 export {default as FormatterBuilder} from './formatter/builder'
+export {default as JsonFormatter} from './formatter/json_formatter'
+export {default as PrettyFormatter} from './formatter/pretty_formatter'
+export {default as ProgressFormatter} from './formatter/progress_formatter'
+export {default as RerunFormatter} from './formatter/rerun_formatter'
 export {default as Runtime} from './runtime'
 export {default as ScenarioFilter} from './scenario_filter'
+export {default as SnippetsFormatter} from './formatter/snippets_formatter'
 export {default as Status} from './status'
 export {default as SummaryFormatter} from './formatter/summary_formatter'
 export {default as SupportCodeLibraryBuilder} from './support_code_library/builder'
+export {default as UsageFormatter} from './formatter/usage_formatter'
+export {default as UsageJsonFormatter} from './formatter/usage_json_formatter'
 
 export const defineSupportCode = SupportCodeFns.add
 export const getSupportCodeFns = SupportCodeFns.get
