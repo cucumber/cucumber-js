7aa25635a1d9748869a7dba6038d5380c7981c23
Julien Biezemans <jb@jbpros.com>
2017-06-28 23:16:32 +0200

    Modernize example/example.js

--- example/example.js
+++ example/example.js
@@ -1,76 +1,76 @@
-var featureEditor, stepDefinitionsEditor, $output;
+let featureEditor, stepDefinitionsEditor, $output
 
 function runFeature() {
-  $output.empty();
-  $('a[href="#output-tab"]').tab("show");
+  $output.empty()
+  $('a[href="#output-tab"]').tab('show')
 
-  var featureSource = featureEditor.getValue();
-  var feature = Cucumber.FeatureParser.parse({
+  let featureSource = featureEditor.getValue()
+  let feature = Cucumber.FeatureParser.parse({
     scenarioFilter: new Cucumber.ScenarioFilter({}),
     source: featureSource,
-    uri: "/feature"
-  });
+    uri: '/feature'
+  })
 
-  Cucumber.clearSupportCodeFns();
-  new Function(stepDefinitionsEditor.getValue())();
-  var supportCodeLibrary = Cucumber.SupportCodeLibraryBuilder.build({
-    cwd: "/",
+  Cucumber.clearSupportCodeFns()
+  new Function(stepDefinitionsEditor.getValue())()
+  let supportCodeLibrary = Cucumber.SupportCodeLibraryBuilder.build({
+    cwd: '/',
     fns: Cucumber.getSupportCodeFns()
-  });
+  })
 
-  var formatterOptions = {
+  let formatterOptions = {
     colorsEnabled: true,
-    cwd: "/",
-    log: function(data) {
-      appendToOutput(ansiHTML(data));
+    cwd: '/',
+    log(data) {
+      appendToOutput(ansiHTML(data))
     },
-    supportCodeLibrary: supportCodeLibrary
-  };
-  var prettyFormatter = Cucumber.FormatterBuilder.build(
-    "pretty",
+    supportCodeLibrary
+  }
+  let prettyFormatter = Cucumber.FormatterBuilder.build(
+    'pretty',
     formatterOptions
-  );
+  )
 
-  var runtime = new Cucumber.Runtime({
+  let runtime = new Cucumber.Runtime({
     features: [feature],
     listeners: [prettyFormatter],
-    supportCodeLibrary: supportCodeLibrary
-  });
-  return runtime.start();
+    supportCodeLibrary
+  })
+  return runtime.start()
 }
 
 function appendToOutput(data) {
-  $output.append(data);
-  $output.scrollTop($output.prop("scrollHeight"));
+  $output.append(data)
+  $output.scrollTop($output.prop('scrollHeight'))
 }
 
 function displayError(error) {
-  var errorContainer = $("<div>");
-  errorContainer.addClass("error").text(error.stack || error);
-  appendToOutput(errorContainer);
+  let errorContainer = $('<div>')
+  errorContainer.addClass('error').text(error.stack || error)
+  appendToOutput(errorContainer)
 }
 
 $(function() {
-  featureEditor = ace.edit("feature");
-  featureEditor.getSession().setMode("ace/mode/gherkin");
+  featureEditor = ace.edit('feature')
+  featureEditor.getSession().setMode('ace/mode/gherkin')
 
-  stepDefinitionsEditor = ace.edit("step-definitions");
-  stepDefinitionsEditor.getSession().setMode("ace/mode/javascript");
+  stepDefinitionsEditor = ace.edit('step-definitions')
+  stepDefinitionsEditor.getSession().setMode('ace/mode/javascript')
 
-  $output = $("#output");
+  $output = $('#output')
 
-  window.onerror = displayError;
+  window.onerror = displayError
 
-  $("#run-feature").click(function() {
+  $('#run-feature').click(function() {
     runFeature()
       .then(function(success) {
-        var exitStatus = success ? "0" : "1";
-        var exitStatusContainer = $("<div>");
+        let exitStatus = success ? '0' : '1'
+        let exitStatusContainer = $('<div>')
         exitStatusContainer
-          .addClass("exit-status")
-          .text("Exit Status: " + exitStatus);
-        appendToOutput(exitStatusContainer);
+          .addClass('exit-status')
+          .text('Exit Status: ' + exitStatus)
+        appendToOutput(exitStatusContainer)
       })
-      .catch(displayError);
-  });
-});
+      .catch(displayError)
+  })
+})
