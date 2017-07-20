e661d6f67f18c867a7c653c069f1903684ef460e
Charles Rudolph <charles.w.rudolph@gmail.com>
2017-07-19 21:15:31 -0700

    miscellaneous updates

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import SupportCodeFns from './support_code_fns'
 import * as formatterHelpers from './formatter/helpers'
 
 export { default as Cli } from './cli'
-export { default as FeatureParser } from './cli/feature_parser'
 export { default as Formatter } from './formatter'
 export { default as FormatterBuilder } from './formatter/builder'
 export { default as JsonFormatter } from './formatter/json_formatter'
 export { default as ProgressFormatter } from './formatter/progress_formatter'
 export { default as RerunFormatter } from './formatter/rerun_formatter'
 export { default as Runtime } from './runtime'
-export { default as ScenarioFilter } from './scenario_filter'
+export { default as PickleFilter } from './pickle_filter'
 export { default as SnippetsFormatter } from './formatter/snippets_formatter'
 export { default as Status } from './status'
 export { default as SummaryFormatter } from './formatter/summary_formatter'
@@ -35,8 +34,6 @@ export const After = proxySupportCode('After')
 export const AfterAll = proxySupportCode('AfterAll')
 export const Before = proxySupportCode('Before')
 export const BeforeAll = proxySupportCode('BeforeAll')
-export const registerHandler = proxySupportCode('registerHandler')
-export const registerListener = proxySupportCode('registerListener')
 export const setDefaultTimeout = proxySupportCode('setDefaultTimeout')
 export const setDefinitionFunctionWrapper = proxySupportCode(
   'setDefinitionFunctionWrapper'
