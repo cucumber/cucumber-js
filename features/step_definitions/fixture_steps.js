f41255f8250a09d348c4ce744b1d78a19d3de69d
Charles Rudolph <charles.w.rudolph@gmail.com>
2017-08-04 07:23:41 -0700

    update

--- features/step_definitions/fixture_steps.js
+++ features/step_definitions/fixture_steps.js
@@ -7,9 +7,7 @@ import fs from 'mz/fs'
 import path from 'path'
 
 defineSupportCode(function({ Then }) {
-  Then('the output matches the fixture {stringInDoubleQuotes}', async function(
-    filePath
-  ) {
+  Then('the output matches the fixture {string}', async function(filePath) {
     const fixturePath = path.join(__dirname, '..', 'fixtures', filePath)
     const expected = await fs.readFile(fixturePath, 'utf8')
     const normalizedActual = normalizeEventProtocolOutput(this.lastRun.output)
