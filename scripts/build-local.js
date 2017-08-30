977cdd9694e7acdfff2a35e6c0f249e69ce39095
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-08-30 21:20:39 +0100

    Update comments

--- scripts/build-local.js
+++ scripts/build-local.js
@@ -2,14 +2,15 @@ var path = require('path')
 var fs = require('fs')
 var spawn = require('child_process').spawn
 
-// Runs `npm run build-local` if the lib dir is absent
+// If installed as an unreleased git dependency, installs the dev dependencies
+// and compiles the source.
 
 fs.access(path.join(__dirname, '..', 'lib'), function(err) {
   if (err) {
-    // If we're installed as a dependency in another project, install our own node_modules,
-    // which are required to run build-local
+    // The lib dir doesn't exist, which means we need to compile sources with babel.
     fs.access(path.join(__dirname, '..', 'node_modules', 'babel-plugin-external-helpers'), function(err) {
       if(err) {
+        // Looks like we don't have babel - let's install all dev dependencies
         var npmInstall = spawn('npm', ['install'], { cwd: path.join(__dirname, '..'), stdio: 'inherit' })
         npmInstall.on('close', function(code) {
           if(code !== 0) throw new Error('cucumber: `npm install` failed')
