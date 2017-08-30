95778d0f1d4dacdbd85b5866cb6108d1ea9b736f
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-08-30 21:27:18 +0100

    Simplify build-non-release

--- scripts/build-non-release.js
+++ scripts/build-non-release.js
@@ -8,24 +8,14 @@ var spawn = require('child_process').spawn
 fs.access(path.join(__dirname, '..', 'lib'), function(err) {
   if (err) {
     // The lib dir doesn't exist, which means we need to compile sources with babel.
-    fs.access(path.join(__dirname, '..', 'node_modules', 'babel-plugin-external-helpers'), function(err) {
-      if(err) {
-        // Looks like we don't have babel - let's install all dev dependencies
-        var npmInstall = spawn('npm', ['install'], { cwd: path.join(__dirname, '..'), stdio: 'inherit' })
-        npmInstall.on('close', function(code) {
-          if(code !== 0) throw new Error('cucumber: `npm install` failed')
-          buildLocal()
-        })
-      } else {
-        buildLocal()
-      }
-    })
-
-    function buildLocal() {
+    // Install dev dependencies first (so we can run babel)
+    var npmInstall = spawn('npm', ['install'], { cwd: path.join(__dirname, '..'), stdio: 'inherit' })
+    npmInstall.on('close', function(code) {
+      if(code !== 0) throw new Error('cucumber: `npm install` failed')
       var build = spawn('npm', ['run', 'build-local'], { cwd: path.join(__dirname, '..'), stdio: 'inherit' })
       build.on('close', function(code) {
         if(code !== 0) throw new Error('cucumber: `npm run build-local` failed')
       })
-    }
+    })
   }
 })
