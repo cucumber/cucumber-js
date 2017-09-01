4109f58de9539f613d69486bf7714e48df107804
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-09-02 00:22:24 +0100

    Don't use yarn

--- scripts/build-non-release.js
+++ scripts/build-non-release.js
@@ -3,10 +3,7 @@ var fs = require('fs')
 var spawn = require('child_process').spawn
 
 // If installed as an unreleased git dependency, installs the dev dependencies
-// and compiles the source. Uses yarn by default. Set `CUCUMBER_UNRELEASED_BUILD_TOOL=npm`
-// if you prefer to use npm (it's slower).
-
-const buildTool = process.env.CUCUMBER_UNRELEASED_BUILD_TOOL || 'yarn'
+// and compiles the source.
 
 fs.access(path.join(__dirname, '..', 'lib'), function(err) {
   if (err) {
@@ -14,9 +11,9 @@ fs.access(path.join(__dirname, '..', 'lib'), function(err) {
     fs.access(path.join(__dirname, '..', 'node_modules', 'babel-plugin-external-helpers'), function(err) {
       if(err) {
         // Looks like we don't have babel - let's install all dev dependencies
-        var npmInstall = spawn(buildTool, ['install'], { cwd: path.join(__dirname, '..'), stdio: 'inherit' })
+        var npmInstall = spawn('npm', ['install'], { cwd: path.join(__dirname, '..'), stdio: 'inherit' })
         npmInstall.on('close', function(code) {
-          if(code !== 0) throw new Error('cucumber: `' + buildTool + ' install` failed')
+          if(code !== 0) throw new Error('cucumber: `npm install` failed')
           buildLocal()
         })
       } else {
@@ -25,9 +22,9 @@ fs.access(path.join(__dirname, '..', 'lib'), function(err) {
     })
 
     function buildLocal() {
-      var build = spawn(buildTool, ['run', 'build-local'], { cwd: path.join(__dirname, '..'), stdio: 'inherit' })
+      var build = spawn('npm', ['run', 'build-local'], { cwd: path.join(__dirname, '..'), stdio: 'inherit' })
       build.on('close', function(code) {
-        if(code !== 0) throw new Error('cucumber: `' + buildTool + ' run build-local` failed')
+        if(code !== 0) throw new Error('cucumber: `npm run build-local` failed')
       })
     }
   }
