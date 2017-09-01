514457cfe14f7dc0c556c7b7b4eef265e596015f
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-09-02 00:26:36 +0100

    Prevent scripts/build-non-release.js from running more than once

--- scripts/build-non-release.js
+++ scripts/build-non-release.js
@@ -5,27 +5,33 @@ var spawn = require('child_process').spawn
 // If installed as an unreleased git dependency, installs the dev dependencies
 // and compiles the source.
 
-fs.access(path.join(__dirname, '..', 'lib'), function(err) {
-  if (err) {
-    // The lib dir doesn't exist, which means we need to compile sources with babel.
-    fs.access(path.join(__dirname, '..', 'node_modules', 'babel-plugin-external-helpers'), function(err) {
-      if(err) {
-        // Looks like we don't have babel - let's install all dev dependencies
-        var npmInstall = spawn('npm', ['install'], { cwd: path.join(__dirname, '..'), stdio: 'inherit' })
-        npmInstall.on('close', function(code) {
-          if(code !== 0) throw new Error('cucumber: `npm install` failed')
+if(!process.env.building_non_release) {
+  fs.access(path.join(__dirname, '..', 'lib'), function(err) {
+    if (err) {
+      // The lib dir doesn't exist, which means we need to compile sources with babel.
+      fs.access(path.join(__dirname, '..', 'node_modules', 'babel-plugin-external-helpers'), function(err) {
+        if(err) {
+          // Looks like we don't have babel - let's install all dev dependencies
+          var npmInstall = spawn('npm', ['install'], {
+            cwd: path.join(__dirname, '..'),
+            stdio: 'inherit',
+            env: Object.assign(process.env, { building_non_release: '1' })
+          })
+          npmInstall.on('close', function(code) {
+            if(code !== 0) throw new Error('cucumber: `npm install` failed')
+            buildLocal()
+          })
+        } else {
           buildLocal()
-        })
-      } else {
-        buildLocal()
-      }
-    })
-
-    function buildLocal() {
-      var build = spawn('npm', ['run', 'build-local'], { cwd: path.join(__dirname, '..'), stdio: 'inherit' })
-      build.on('close', function(code) {
-        if(code !== 0) throw new Error('cucumber: `npm run build-local` failed')
+        }
       })
+
+      function buildLocal() {
+        var build = spawn('npm', ['run', 'build-local'], { cwd: path.join(__dirname, '..'), stdio: 'inherit' })
+        build.on('close', function(code) {
+          if(code !== 0) throw new Error('cucumber: `npm run build-local` failed')
+        })
+      }
     }
-  }
-})
+  })
+}
