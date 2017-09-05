7bb8dd22d9c9fa9afbe12f56746620624c37f9f2
Charles Rudolph <charles.w.rudolph@gmail.com>
2017-09-05 08:15:22 -0700

    fix

--- src/formatter/usage_formatter_spec.js
+++ src/formatter/usage_formatter_spec.js
@@ -23,12 +23,56 @@ describe('UsageFormatter', function() {
     })
 
     describe('no step definitions', function() {
-      beforeEach(function() {
-        this.eventBroadcaster.emit('test-run-finished')
+      describe('without steps', function() {
+        beforeEach(function() {
+          this.eventBroadcaster.emit('test-run-finished')
+        })
+
+        it('outputs "No step definitions"', function() {
+          expect(this.output).to.eql('No step definitions')
+        })
       })
 
-      it('outputs "No step definitions"', function() {
-        expect(this.output).to.eql('No step definitions')
+      describe('with a step', function() {
+        beforeEach(function() {
+          const events = Gherkin.generateEvents(
+            'Feature: a\nScenario: b\nWhen abc\nThen ab',
+            'a.feature'
+          )
+          events.forEach(event => {
+            this.eventBroadcaster.emit(event.type, event)
+            if (event.type === 'pickle') {
+              this.eventBroadcaster.emit('pickle-accepted', {
+                type: 'pickle-accepted',
+                pickle: event.pickle,
+                uri: event.uri
+              })
+            }
+          })
+          const testCase = { sourceLocation: { uri: 'a.feature', line: 2 } }
+          this.eventBroadcaster.emit('test-case-prepared', {
+            ...testCase,
+            steps: [
+              { sourceLocation: { uri: 'a.feature', line: 3 } },
+              { sourceLocation: { uri: 'a.feature', line: 4 } }
+            ]
+          })
+          this.eventBroadcaster.emit('test-step-finished', {
+            index: 0,
+            testCase,
+            result: {}
+          })
+          this.eventBroadcaster.emit('test-step-finished', {
+            index: 1,
+            testCase,
+            result: {}
+          })
+          this.eventBroadcaster.emit('test-run-finished')
+        })
+
+        it('outputs "No step definitions"', function() {
+          expect(this.output).to.eql('No step definitions')
+        })
       })
     })
 
