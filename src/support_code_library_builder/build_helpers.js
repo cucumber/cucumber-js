02933efb22fd60c72eed6db5183c15a6db98a871
Charles Rudolph <charles.rudolph@originate.com>
2018-04-11 14:43:06 -0700

    cleanup support code library

---
+++ src/support_code_library_builder/build_helpers.js
@@ -0,0 +1,122 @@
+import { deprecate } from 'util'
+import _ from 'lodash'
+import { formatLocation } from '../formatter/helpers'
+import { ParameterType } from 'cucumber-expressions'
+import path from 'path'
+import StackTrace from 'stacktrace-js'
+import StepDefinition from '../models/step_definition'
+import TestCaseHookDefinition from '../models/test_case_hook_definition'
+import TestRunHookDefinition from '../models/test_run_hook_definition'
+import validateArguments from './validate_arguments'
+
+export function buildTestCaseHookDefinition({ options, code, cwd }) {
+  if (typeof options === 'string') {
+    options = { tags: options }
+  } else if (typeof options === 'function') {
+    code = options
+    options = {}
+  }
+  const { line, uri } = getDefinitionLineAndUri(cwd)
+  validateArguments({
+    args: { code, options },
+    fnName: 'defineTestCaseHook',
+    location: formatLocation({ line, uri }),
+  })
+  return new TestCaseHookDefinition({
+    code,
+    line,
+    options,
+    uri,
+  })
+}
+
+export function buildTestRunHookDefinition({ options, code, cwd }) {
+  if (typeof options === 'string') {
+    options = { tags: options }
+  } else if (typeof options === 'function') {
+    code = options
+    options = {}
+  }
+  const { line, uri } = getDefinitionLineAndUri(cwd)
+  validateArguments({
+    args: { code, options },
+    fnName: 'defineTestRunHook',
+    location: formatLocation({ line, uri }),
+  })
+  return new TestRunHookDefinition({
+    code,
+    line,
+    options,
+    uri,
+  })
+}
+
+export function buildStepDefinition({ pattern, options, code, cwd }) {
+  if (typeof options === 'function') {
+    code = options
+    options = {}
+  }
+  const { line, uri } = getDefinitionLineAndUri(cwd)
+  validateArguments({
+    args: { code, pattern, options },
+    fnName: 'defineStep',
+    location: formatLocation({ line, uri }),
+  })
+  return new StepDefinition({
+    code,
+    line,
+    options,
+    pattern,
+    uri,
+  })
+}
+
+const projectPath = path.join(__dirname, '..', '..')
+const projectSrcPath = path.join(projectPath, 'src')
+const projectLibPath = path.join(projectPath, 'lib')
+
+function getDefinitionLineAndUri(cwd) {
+  let line = 'unknown'
+  let uri = 'unknown'
+  const stackframes = StackTrace.getSync()
+  const stackframe = _.find(stackframes, frame => {
+    const filename = frame.getFileName()
+    return (
+      !_.includes(filename, projectSrcPath) &&
+      !_.includes(filename, projectLibPath)
+    )
+  })
+  if (stackframe) {
+    line = stackframe.getLineNumber()
+    uri = stackframe.getFileName()
+    if (uri) {
+      uri = path.relative(cwd, uri)
+    }
+  }
+  return { line, uri }
+}
+
+export function buildParameterType({
+  name,
+  typeName,
+  regexp,
+  transformer,
+  useForSnippets,
+  preferForRegexpMatch,
+}) {
+  const getTypeName = deprecate(
+    () => typeName,
+    'Cucumber defineParameterType: Use name instead of typeName'
+  )
+  const _name = name || getTypeName()
+  if (typeof useForSnippets !== 'boolean') useForSnippets = true
+  if (typeof preferForRegexpMatch !== 'boolean') preferForRegexpMatch = false
+  return new ParameterType(
+    _name,
+    regexp,
+    null,
+    transformer,
+    useForSnippets,
+    preferForRegexpMatch
+  )
+}
--- src/support_code_library_builder/index.js
+++ src/support_code_library_builder/index.js
@@ -2,22 +2,22 @@ import _ from 'lodash'
 import util from 'util'
 import TransformLookupBuilder from './parameter_type_registry_builder'
 import {
-  defineTestRunHook,
-  defineParameterType,
-  defineTestCaseHook,
-  defineStep,
-} from './define_helpers'
+  buildParameterType,
+  buildStepDefinition,
+  buildTestCaseHookDefinition,
+  buildTestRunHookDefinition,
+} from './build_helpers'
 import { wrapDefinitions } from './finalize_helpers'
 
 export class SupportCodeLibraryBuilder {
   constructor() {
     this.methods = {
-      defineParameterType: defineParameterType(this),
-      After: defineTestCaseHook(this, 'afterTestCaseHookDefinitions'),
-      AfterAll: defineTestRunHook(this, 'afterTestRunHookDefinitions'),
-      Before: defineTestCaseHook(this, 'beforeTestCaseHookDefinitions'),
-      BeforeAll: defineTestRunHook(this, 'beforeTestRunHookDefinitions'),
-      defineStep: defineStep(this),
+      defineParameterType: this.defineParameterType.bind(this),
+      After: this.defineTestCaseHook('afterTestCaseHookDefinitions'),
+      AfterAll: this.defineTestRunHook('afterTestRunHookDefinitions'),
+      Before: this.defineTestCaseHook('beforeTestCaseHookDefinitions'),
+      BeforeAll: this.defineTestRunHook('beforeTestRunHookDefinitions'),
+      defineStep: this.defineStep.bind(this),
       defineSupportCode: util.deprecate(fn => {
         fn(this.methods)
       }, 'cucumber: defineSupportCode is deprecated. Please require/import the individual methods instead.'),
@@ -32,6 +32,43 @@ export class SupportCodeLibraryBuilder {
       },
     }
     this.methods.Given = this.methods.When = this.methods.Then = this.methods.defineStep
+  }
+
+  defineParameterType(options) {
+    const parameterType = buildParameterType(options)
+    this.options.parameterTypeRegistry.defineParameterType(parameterType)
+  }
+
+  defineStep(pattern, options, code) {
+    const stepDefinition = buildStepDefinition({
+      pattern,
+      options,
+      code,
+      cwd: this.cwd,
+    })
+    this.options.stepDefinitions.push(stepDefinition)
+  }
+
+  defineTestCaseHook(collectionName) {
+    return (options, code) => {
+      const hookDefinition = buildTestCaseHookDefinition({
+        options,
+        code,
+        cwd: this.cwd,
+      })
+      this.options[collectionName].push(hookDefinition)
+    }
+  }
+
+  defineTestRunHook(collectionName) {
+    return (options, code) => {
+      const hookDefinition = buildTestRunHookDefinition({
+        options,
+        code,
+        cwd: this.cwd,
+      })
+      this.options[collectionName].push(hookDefinition)
+    }
   }
 
   finalize() {
