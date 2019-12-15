0e8d05762c2e68947470132c97b7db7e664aaee9
Charles Rudolph <charles.w.rudolph@gmail.com>
2019-12-15 11:11:27 -0800

    fix issue helpers test

--- src/formatter/helpers/issue_helpers_spec.js
+++ src/formatter/helpers/issue_helpers_spec.js
@@ -36,219 +36,211 @@ describe('IssueHelpers', () => {
     describe('with a failed step', () => {
       it('prints the scenario', async () => {
         // Arrange
-        const sourceData = [
-          'Feature: my feature',
-          '  Scenario: my scenario',
-          '    Given a passing step',
-          '    When a failing step',
-          '    Then a passing step',
-        ].join('\n')
+        const sourceData = `\
+Feature: my feature
+  Scenario: my scenario
+    Given a passing step
+    When a failing step
+    Then a passing step
+`
 
         // Act
         const output = await testFormatIssue(sourceData)
 
         // Assert
-        expect(output).to.eql(
-          [
-            '1) Scenario: my scenario # a.feature:2',
-            `   ${figures.tick} Given a passing step # steps.js:27`,
-            `   ${figures.cross} When a failing step # steps.js:7`,
-            '       error',
-            '   - Then a passing step # steps.js:27',
-            '',
-            '',
-          ].join('\n')
-        )
+        expect(output).to.eql(`\
+1) Scenario: my scenario # a.feature:2
+   ${figures.tick} Given a passing step # steps.js:27
+   ${figures.cross} When a failing step # steps.js:7
+       error
+   - Then a passing step # steps.js:27
+
+`)
       })
     })
 
     describe('with an ambiguous step', () => {
       it('returns the formatted scenario', async () => {
         // Arrange
-        const sourceData = [
-          'Feature: my feature',
-          '  Scenario: my scenario',
-          '    Given a passing step',
-          '    When an ambiguous step',
-          '    Then a passing step',
-        ].join('\n')
+        const sourceData = `\
+Feature: my feature
+  Scenario: my scenario
+    Given a passing step
+    When an ambiguous step
+    Then a passing step
+`
 
         // Act
         const output = await testFormatIssue(sourceData)
 
         // Assert
-        expect(output).to.eql(
-          [
-            '1) Scenario: my scenario # a.feature:2',
-            `   ${figures.tick} Given a passing step # steps.js:27`,
-            `   ${figures.cross} When an ambiguous step`,
-            '       Multiple step definitions match:',
-            '         an ambiguous step    - steps.js:11',
-            '         /an? ambiguous step/ - steps.js:12',
-            '   - Then a passing step # steps.js:27',
-            '',
-            '',
-          ].join('\n')
-        )
+        expect(output).to.eql(`\
+1) Scenario: my scenario # a.feature:2
+   ${figures.tick} Given a passing step # steps.js:27
+   ${figures.cross} When an ambiguous step
+       Multiple step definitions match:
+         an ambiguous step    - steps.js:11
+         /an? ambiguous step/ - steps.js:12
+   - Then a passing step # steps.js:27
+
+`)
       })
     })
 
     describe('with an undefined step', () => {
       it('returns the formatted scenario', async () => {
         // Arrange
-        const sourceData = [
-          'Feature: my feature',
-          '  Scenario: my scenario',
-          '    Given a passing step',
-          '    When an undefined step',
-          '    Then a passing step',
-        ].join('\n')
+        const sourceData = `\
+Feature: my feature
+  Scenario: my scenario
+    Given a passing step
+    When an undefined step
+    Then a passing step
+`
 
         // Act
         const output = await testFormatIssue(sourceData)
 
         // Assert
-        expect(output).to.eql(
-          [
-            '1) Scenario: my scenario # a.feature:2',
-            `   ${figures.tick} Given a passing step # steps.js:27`,
-            `   ? When an undefined step`,
-            '       Undefined. Implement with the following snippet:',
-            '',
-            "         When('an undefined step', function () {",
-            '           // Write code here that turns the phrase above into concrete actions',
-            "           return 'pending';",
-            '         });',
-            '',
-            '   - Then a passing step # steps.js:27',
-            '',
-            '',
-          ].join('\n')
-        )
+        expect(output).to.eql(`\
+1) Scenario: my scenario # a.feature:2
+   ${figures.tick} Given a passing step # steps.js:27
+   ? When an undefined step
+       Undefined. Implement with the following snippet:
+
+         When('an undefined step', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+   - Then a passing step # steps.js:27
+
+`)
       })
     })
 
     describe('with a pending step', () => {
       it('returns the formatted scenario', async () => {
         // Arrange
-        const sourceData = [
-          'Feature: my feature',
-          '  Scenario: my scenario',
-          '    Given a passing step',
-          '    When a pending step',
-          '    Then a passing step',
-        ].join('\n')
+        const sourceData = `\
+Feature: my feature
+  Scenario: my scenario
+    Given a passing step
+    When a pending step
+    Then a passing step
+`
 
         // Act
         const output = await testFormatIssue(sourceData)
 
         // Assert
-        expect(output).to.eql(
-          [
-            '1) Scenario: my scenario # a.feature:2',
-            `   ${figures.tick} Given a passing step # steps.js:27`,
-            `   ? When a pending step # steps.js:14`,
-            '       Pending',
-            '   - Then a passing step # steps.js:27',
-            '',
-            '',
-          ].join('\n')
-        )
+        expect(output).to.eql(`\
+1) Scenario: my scenario # a.feature:2
+   ${figures.tick} Given a passing step # steps.js:27
+   ? When a pending step # steps.js:14
+       Pending
+   - Then a passing step # steps.js:27
+
+`)
       })
     })
 
     describe('step with data table', () => {
       it('returns the formatted scenario', async () => {
         // Arrange
-        const sourceData = [
-          'Feature: my feature',
-          '  Scenario: my scenario',
-          '    Given a passing step',
-          '    When a pending step',
-          '    Then a passing step',
-          '      |aaa|b|c|',
-          '      |d|e|ff|',
-          '      |gg|h|iii|',
-        ].join('\n')
+        const sourceData = `\
+Feature: my feature
+  Scenario: my scenario
+    Given a passing step
+    When a pending step
+    Then a passing step
+      |aaa|b|c|
+      |d|e|ff|
+      |gg|h|iii|
+`
 
         // Act
         const output = await testFormatIssue(sourceData)
 
         // Assert
-        expect(output).to.eql(
-          [
-            '1) Scenario: my scenario # a.feature:2',
-            `   ${figures.tick} Given a passing step # steps.js:27`,
-            `   ? When a pending step # steps.js:14`,
-            '       Pending',
-            '   - Then a passing step # steps.js:27',
-            '       | aaa | b | c   |',
-            '       | d   | e | ff  |',
-            '       | gg  | h | iii |',
-            '',
-            '',
-          ].join('\n')
-        )
+        expect(output).to.eql(`\
+1) Scenario: my scenario # a.feature:2
+   ${figures.tick} Given a passing step # steps.js:27
+   ? When a pending step # steps.js:14
+       Pending
+   - Then a passing step # steps.js:27
+       | aaa | b | c   |
+       | d   | e | ff  |
+       | gg  | h | iii |
+
+`)
       })
     })
 
     describe('step with doc string', () => {
       it('returns the formatted scenario', async () => {
         // Arrange
-        const sourceData = [
-          'Feature: my feature',
-          '  Scenario: my scenario',
-          '    Given a passing step',
-          '    When a pending step',
-          '    Then a passing step',
-          '       """',
-          '       this is a multiline',
-          '       doc string',
-          '',
-          '       :-)',
-          '       """',
-        ].join('\n')
+        const sourceData = `\
+Feature: my feature
+  Scenario: my scenario
+    Given a passing step
+    When a pending step
+    Then a passing step
+       """
+       this is a multiline
+       doc string
+
+       :-)
+       """
+`
 
         // Act
         const output = await testFormatIssue(sourceData)
 
         // Assert
-        expect(output).to.eql(
-          [
-            '1) Scenario: my scenario # a.feature:2',
-            `   ${figures.tick} Given a passing step # steps.js:27`,
-            `   ? When a pending step # steps.js:14`,
-            '       Pending',
-            '   - Then a passing step # steps.js:27',
-            '       """',
-            '       this is a multiline',
-            '       doc string',
-            '',
-            '       :-)',
-            '       """',
-            '',
-            '',
-          ].join('\n')
-        )
+        expect(output).to.eql(`\
+1) Scenario: my scenario # a.feature:2
+   ${figures.tick} Given a passing step # steps.js:27
+   ? When a pending step # steps.js:14
+       Pending
+   - Then a passing step # steps.js:27
+       """
+       this is a multiline
+       doc string
+
+       :-)
+       """
+
+`)
       })
     })
 
     describe('step with attachment text', () => {
-      it('prints the scenario', function() {
+      it('prints the scenario', async () => {
         // Arrange
+        const sourceData = `\
+Feature: my feature
+  Scenario: my scenario
+    Given attachment step1
+    When attachment step2
+    Then a passing step
+`
         // Act
+        const output = await testFormatIssue(sourceData)
+
         // Assert
-        // expect(output).to.eql(
-        //   '1) Scenario: my scenario # a.feature:2\n' +
-        //     `   ${figures.tick} Given step1 # steps.js:2\n` +
-        //     `       Attachment (text/plain): Some info.\n` +
-        //     `       Attachment (application/json)\n` +
-        //     `       Attachment (image/png)\n` +
-        //     `   ${figures.cross} When step2 # steps.js:3\n` +
-        //     `       Attachment (text/plain): Other info.\n` +
-        //     '       error\n' +
-        //     '   - Then step3 # steps.js:4\n\n'
-        // )
+        expect(output).to.eql(`\
+1) Scenario: my scenario # a.feature:2
+   ${figures.tick} Given attachment step1 # steps.js:33
+       Attachment (text/plain): Some info
+       Attachment (application/json)
+       Attachment (image/png)
+   ${figures.cross} When attachment step2 # steps.js:39
+       Attachment (text/plain): Other info
+       error
+   - Then a passing step # steps.js:27
+
+`)
       })
     })
   })
--- test/fixtures/steps.js
+++ test/fixtures/steps.js
@@ -29,5 +29,16 @@ export function getBaseSupportCodeLibrary() {
     Given('a skipped step', function() {
       return 'skipped'
     })
+
+    Given('attachment step1', function() {
+      this.attach('Some info')
+      this.attach('{"name": "some JSON"}', 'application/json')
+      this.attach(Buffer.from([137, 80, 78, 71]), 'image/png')
+    })
+
+    Given('attachment step2', function() {
+      this.attach('Other info')
+      throw 'error' // eslint-disable-line no-throw-literal
+    })
   })
 }
