8428a770ff7dbeeb2d21f9f2ca9cf567558fcd5c
David Goss <dvdgoss@gmail.com>
2020-01-29 00:17:08 +0000

    add acceptance test for message and json format on failure from rule

---
+++ features/fixtures/formatters/failed-rule.message.json.ts
@@ -0,0 +1,147 @@
+module.exports = [
+  {
+    source: {
+      uri: 'features/a.feature',
+      data:
+        'Feature: a feature\n  Rule: a rule\n    Example: an example\n      Given a step',
+      media: {
+        encoding: 'UTF8',
+        contentType: 'text/x.cucumber.gherkin+plain',
+      },
+    },
+  },
+  {
+    gherkinDocument: {
+      uri: 'features/a.feature',
+      feature: {
+        location: {
+          line: 1,
+          column: 1,
+        },
+        language: 'en',
+        keyword: 'Feature',
+        name: 'a feature',
+        children: [
+          {
+            rule: {
+              location: {
+                line: 2,
+                column: 3,
+              },
+              keyword: 'Rule',
+              name: 'a rule',
+              children: [
+                {
+                  scenario: {
+                    location: {
+                      line: 3,
+                      column: 5,
+                    },
+                    keyword: 'Example',
+                    name: 'an example',
+                    steps: [
+                      {
+                        location: {
+                          line: 4,
+                          column: 7,
+                        },
+                        keyword: 'Given ',
+                        text: 'a step',
+                        id: '1',
+                      },
+                    ],
+                    id: '2',
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  },
+  {
+    pickle: {
+      id: '4',
+      uri: 'features/a.feature',
+      name: 'an example',
+      language: 'en',
+      steps: [
+        {
+          text: 'a step',
+          id: '3',
+          astNodeIds: ['1'],
+        },
+      ],
+      astNodeIds: ['2'],
+    },
+  },
+  {
+    pickleAccepted: {
+      pickleId: '4',
+    },
+  },
+  {
+    testRunStarted: {},
+  },
+  {
+    testCase: {
+      id: '5',
+      pickleId: '4',
+      testSteps: [
+        {
+          id: '6',
+          pickleStepId: '3',
+          stepDefinitionIds: ['0'],
+        },
+      ],
+    },
+  },
+  {
+    testCaseStarted: {
+      attempt: 0,
+      testCaseId: '5',
+      id: '7',
+    },
+  },
+  {
+    testStepStarted: {
+      testStepId: '6',
+      testCaseStartedId: '7',
+    },
+  },
+  {
+    testStepFinished: {
+      testResult: {
+        status: 'FAILED',
+        message:
+          'Error: my error\n    at World.<anonymous> (features/step_definitions/steps.js:3:49)',
+        duration: {
+          seconds: '0',
+          nanos: 0,
+        },
+      },
+      testStepId: '6',
+      testCaseStartedId: '7',
+    },
+  },
+  {
+    testCaseFinished: {
+      testResult: {
+        status: 'FAILED',
+        message:
+          'Error: my error\n    at World.<anonymous> (features/step_definitions/steps.js:3:49)',
+        duration: {
+          seconds: '0',
+          nanos: 0,
+        },
+      },
+      testCaseStartedId: '7',
+    },
+  },
+  {
+    testRunFinished: {
+      success: false,
+    },
+  },
+]
--- features/formatters.feature
+++ features/formatters.feature
@@ -11,6 +11,7 @@ Feature: Formatters
     Then the "message" formatter output matches the fixture "formatters/rejected-pickle-scenario.message.json"
     Then the "json" formatter output matches the fixture "formatters/rejected-pickle.json"
 
+  @rule
   Scenario: rejected pickle from Rule
     Given a file named "features/a.feature" with:
       """
@@ -40,6 +41,7 @@ Feature: Formatters
     Then the "message" formatter output matches the fixture "formatters/passed-scenario.message.json"
     Then the "json" formatter output matches the fixture "formatters/passed-scenario.json"
 
+  @rule
   Scenario: passed from Rule
     Given a file named "features/a.feature" with:
       """
@@ -58,7 +60,7 @@ Feature: Formatters
     Then the "message" formatter output matches the fixture "formatters/passed-rule.message.json"
     Then the "json" formatter output matches the fixture "formatters/passed-rule.json"
 
-  Scenario: failed
+  Scenario: failed from Scenario
     Given a file named "features/a.feature" with:
       """
       Feature: a feature
@@ -72,8 +74,28 @@ Feature: Formatters
       Given(/^a step$/, function(callback) { callback(new Error('my error')) })
       """
     When I run cucumber-js with all formatters
-    Then the "message" formatter output matches the fixture "formatters/failed.message.json"
-    Then the "json" formatter output matches the fixture "formatters/failed.json"
+    Then the "message" formatter output matches the fixture "formatters/failed-scenario.message.json"
+    Then the "json" formatter output matches the fixture "formatters/failed-scenario.json"
+    And it fails
+
+  @rule
+  Scenario: failed from Rule
+    Given a file named "features/a.feature" with:
+      """
+      Feature: a feature
+        Rule: a rule
+          Example: an example
+            Given a step
+      """
+    Given a file named "features/step_definitions/steps.js" with:
+      """
+      const {Given} = require('cucumber')
+
+      Given(/^a step$/, function(callback) { callback(new Error('my error')) })
+      """
+    When I run cucumber-js with all formatters
+    Then the "message" formatter output matches the fixture "formatters/failed-rule.message.json"
+    Then the "json" formatter output matches the fixture "formatters/failed-rule.json"
     And it fails
 
   Scenario: retried and passed
