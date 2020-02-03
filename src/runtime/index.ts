45ba630769f36bf82cdacb4422da8fd40aa83107
David Goss <dvdgoss@gmail.com>
2020-02-03 22:01:01 +0000

    whoops

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -61,7 +61,7 @@ export default class Runtime {
   }
 
   async runTestRunHooks(
-    key: 'beforeTestRunHookDefinitions' | 'beforeTestRunHookDefinitions',
+    key: 'beforeTestRunHookDefinitions' | 'afterTestRunHookDefinitions',
     name: string
   ): Promise<void> {
     if (this.options.dryRun) {
