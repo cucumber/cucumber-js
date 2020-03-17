beb1d578233bdeacedce5d168f60f143cec003bf
David Goss <dvdgoss@gmail.com>
2020-03-17 06:57:07 +0000

    split out new test for rule/example in rerun formatter spec

--- src/formatter/rerun_formatter_spec.ts
+++ src/formatter/rerun_formatter_spec.ts
@@ -180,6 +180,31 @@ describe('RerunFormatter', () => {
                 uri: 'a.feature',
               },
               {
+                data: 'Feature: a\n\nScenario: b\nGiven a step',
+                uri: 'b.feature',
+              },
+            ]
+
+            // Act
+            const output = await testFormatter({
+              parsedArgvOptions,
+              sources,
+              type: 'rerun',
+            })
+
+            // Assert
+            expect(output).to.eql(`a.feature:2${separator.expected}b.feature:3`)
+          })
+
+          it('outputs the reference needed to run the rule example again', async () => {
+            // Arrange
+            const parsedArgvOptions = { rerun: { separator: separator.opt } }
+            const sources = [
+              {
+                data: 'Feature: a\nRule: b\nExample: c\nGiven a step',
+                uri: 'a.feature',
+              },
+              {
                 data: 'Feature: a\n\nRule: b\nExample: c\nGiven a step',
                 uri: 'b.feature',
               },
@@ -193,7 +218,7 @@ describe('RerunFormatter', () => {
             })
 
             // Assert
-            expect(output).to.eql(`a.feature:2${separator.expected}b.feature:4`)
+            expect(output).to.eql(`a.feature:3${separator.expected}b.feature:4`)
           })
         })
       }
