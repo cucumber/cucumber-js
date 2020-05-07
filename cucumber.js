bca4d7dea9d6760af6322ffeeeca562db6d5e4f4
David Goss <dvdgoss@gmail.com>
2020-05-07 22:08:14 +0100

    add a script to run cck tests

--- cucumber.js
+++ cucumber.js
@@ -8,6 +8,16 @@ var common = [
   '--format usage:usage.txt',
 ].join(' ')
 
+var cck = [
+  'node_modules/@cucumber/compatibility-kit/features/**/*.feature',
+  '--require-module ts-node/register',
+  '--require compatibility/**/*.ts',
+  `--format ${
+    process.env.CI || !process.stdout.isTTY ? 'progress' : 'progress-bar'
+  }`,
+].join(' ')
+
 module.exports = {
   default: common,
+  cck,
 }
