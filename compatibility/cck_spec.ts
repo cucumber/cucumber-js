f97930e9300ef659e8fc5e8de0b8c8cdfc32c15f
David Goss <dvdgoss@gmail.com>
2020-05-08 13:10:13 +0100

    test via mocha with one run per feature

---
+++ compatibility/cck_spec.ts
@@ -0,0 +1,48 @@
+import { describe, it } from 'mocha'
+import { expect } from 'chai'
+import globby from 'globby'
+import fs from 'fs'
+import ndjsonParse from 'ndjson-parse'
+import path from 'path'
+import { PassThrough } from 'stream'
+import { Cli } from '../lib'
+import toString from 'stream-to-string'
+
+const PROJECT_PATH = path.join(__dirname, '..')
+const CCK_FEATURES_PATH = 'node_modules/@cucumber/compatibility-kit/features'
+
+describe('Cucumber Compatibility Kit', () => {
+  globby.sync([`${CCK_FEATURES_PATH}/**/*.ndjson`]).forEach(fixturePath => {
+    const suiteName = /^.+\/(.+)\.ndjson$/.exec(fixturePath)[1]
+    it(`passes the cck suite for '${suiteName}'`, async () => {
+      const args = [
+        'node',
+        path.join(PROJECT_PATH, 'bin', 'cucumber-js'),
+      ].concat([
+        `${CCK_FEATURES_PATH}/${suiteName}/${suiteName}.feature`,
+        '--require-module',
+        'ts-node/register',
+        '--require',
+        'compatibility/**/*.ts',
+        '--predictable-ids',
+        '--format',
+        'message',
+      ])
+      const stdout = new PassThrough()
+      try {
+        await new Cli({
+          argv: args,
+          cwd: PROJECT_PATH,
+          stdout,
+        }).run()
+      } catch (ignored) {}
+      stdout.end()
+
+      const actualMessages = ndjsonParse(await toString(stdout))
+      const expectedMessages = ndjsonParse(
+        fs.readFileSync(fixturePath, { encoding: 'utf-8' })
+      )
+      expect(actualMessages).to.deep.eq(expectedMessages)
+    }).timeout(10000)
+  })
+})
--- cucumber.js
+++ cucumber.js
@@ -8,17 +8,6 @@ var common = [
   '--format usage:usage.txt',
 ].join(' ')
 
-var cck = [
-  'node_modules/@cucumber/compatibility-kit/features/**/*.feature',
-  '--require-module ts-node/register',
-  '--require compatibility/**/*.ts',
-  `--format ${
-    process.env.CI || !process.stdout.isTTY ? 'progress' : 'progress-bar'
-  }`,
-  '--format message:compatibility/out.ndjson',
-].join(' ')
-
 module.exports = {
   default: common,
-  cck,
 }
