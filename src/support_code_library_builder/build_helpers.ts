e98bb602b57de12269ea34c3e34aa19a2020c60a
David Goss <dvdgoss@gmail.com>
2020-05-09 22:46:51 +0100

    remove typeName from options object for defineParameterType

--- src/support_code_library_builder/build_helpers.ts
+++ src/support_code_library_builder/build_helpers.ts
@@ -1,14 +1,9 @@
-import { deprecate } from 'util'
 import _ from 'lodash'
 import { ParameterType } from 'cucumber-expressions'
 import path from 'path'
 import StackTrace from 'stacktrace-js'
 import { isFileNameInCucumber } from '../stack_trace_filter'
-import {
-  doesHaveValue,
-  doesNotHaveValue,
-  valueOrDefault,
-} from '../value_checker'
+import { doesHaveValue, valueOrDefault } from '../value_checker'
 import { ILineAndUri } from '../types'
 import { IParameterTypeDefinition } from './types'
 
@@ -34,19 +29,11 @@ export function getDefinitionLineAndUri(cwd: string): ILineAndUri {
 
 export function buildParameterType({
   name,
-  typeName,
   regexp,
   transformer,
   useForSnippets,
   preferForRegexpMatch,
 }: IParameterTypeDefinition<any>): ParameterType<any> {
-  const getTypeName = deprecate(
-    () => typeName,
-    'Cucumber defineParameterType: Use name instead of typeName'
-  )
-  if (doesNotHaveValue(name) && doesHaveValue(typeName)) {
-    name = getTypeName()
-  }
   if (typeof useForSnippets !== 'boolean') useForSnippets = true
   if (typeof preferForRegexpMatch !== 'boolean') preferForRegexpMatch = false
   return new ParameterType(
