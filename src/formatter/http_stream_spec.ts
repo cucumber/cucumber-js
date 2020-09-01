56c0888324a1ad23413755d9e128ca876f15d9ec
Aslak Hellesøy <aslak.hellesoy@smartbear.com>
2020-09-01 13:48:58 +0100

    Start TDDing HttpStream

---
+++ src/formatter/http_stream_spec.ts
@@ -0,0 +1,49 @@
+import { Writable } from 'stream'
+import express from 'express'
+import * as http from 'http'
+import { promisify } from 'util'
+import { Server } from 'net'
+
+class ReportServer {
+  private readonly server: Server
+  constructor(private readonly port: number) {
+    const app = express()
+    this.server = http.createServer(app)
+  }
+
+  async start(): Promise<void> {
+    const listen = promisify(this.server.listen.bind(this.server))
+    await listen(this.port)
+  }
+
+  async stop(): Promise<void> {
+    const close = promisify(this.server.close.bind(this.server))
+    await close()
+  }
+}
+
+class HttpStream extends Writable {
+  constructor(url: string) {
+    super()
+  }
+}
+
+describe('HttpStream', () => {
+  const port = 8998
+  let reportServer: ReportServer
+
+  beforeEach(async () => {
+    reportServer = new ReportServer(port)
+    await reportServer.start()
+  })
+
+  afterEach(async () => {
+    await reportServer.stop()
+  })
+
+  it('sends a request with written data when the stream is closed', async () => {
+    const stream = new HttpStream(`http://localhost:${port}/s3`)
+    stream.write('hello')
+    stream.end()
+  })
+})
