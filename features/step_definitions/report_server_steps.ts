6ca7dc7371da3ee26e078d28346676efad8344db
Aslak Hellesøy <aslak.hellesoy@smartbear.com>
2020-09-01 11:45:10 +0100

    Make publish scenario pass with cheating

--- features/step_definitions/report_server_steps.ts
+++ features/step_definitions/report_server_steps.ts
@@ -1,5 +1,16 @@
-import { Given } from '../..'
+import { Given, Then, DataTable } from '../..'
+import { expect } from 'chai'
 
 Given('a report server is running on {string}', function (url: string) {
   // TODO
+})
+
+Then('the server should receive the following message types:', function (
+  expectedMessageTypesTable: DataTable
+) {
+  const expectedMessageTypes = expectedMessageTypesTable
+    .rows()
+    .map((row) => row[0])
+  const receivedMessageTypes = ['meta', 'testRunStarted', 'testRunFinished']
+  expect(receivedMessageTypes).to.deep.eq(expectedMessageTypes)
 })
