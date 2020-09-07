841d8d6b4ffef2f5a4917fd97e655f4d4a575258
Charles Rudolph <charles.w.rudolph@gmail.com>
2020-09-07 16:20:52 -0700

    revert dep lint config, lint fix banner

--- src/cli/publish_banner.ts
+++ src/cli/publish_banner.ts
@@ -1,27 +1,34 @@
-import colors from 'colors/safe';
-import Table from 'cli-table3';
+import colors from 'colors/safe'
+import Table from 'cli-table3'
 
-const underlineBoldCyan = (x: string) => colors.underline(colors.bold(colors.cyan(x)))
+const underlineBoldCyan = (x: string): string =>
+  colors.underline(colors.bold(colors.cyan(x)))
 
-const text = [
-  'Share your Cucumber Report with your team at ' + underlineBoldCyan('https://reports.cucumber.io'),
-  '',
-  'Command line option:    ' + colors.cyan('--publish'),
-  'Environment variable:   ' + colors.cyan('CUCUMBER_PUBLISH_ENABLED') + '=' + colors.cyan('true'),
-  '',
-  'More information at ' + underlineBoldCyan('https://reports.cucumber.io/docs/cucumber-js'),
-  '',
-  'To disable this message, add this to your ' + colors.bold('./cucumber.js') + ':',
-  colors.bold("module.exports = { default: '--publish-quiet' }")
-].join('\n')
+const formattedReportUrl = underlineBoldCyan('https://reports.cucumber.io')
+const formattedEnv =
+  colors.cyan('CUCUMBER_PUBLISH_ENABLED') + '=' + colors.cyan('true')
+const formattedMoreInfoUrl = underlineBoldCyan(
+  'https://reports.cucumber.io/docs/cucumber-js'
+)
+
+const text = `\
+Share your Cucumber Report with your team at ${formattedReportUrl}
+
+Command line option:    ${colors.cyan('--publish')}
+Environment variable:   ${formattedEnv}
+
+More information at ${formattedMoreInfoUrl}
+
+To disable this message, add this to your ${colors.bold('./cucumber.js')}: 
+${colors.bold("module.exports = { default: '--publish-quiet' }")}`
 
 const table = new Table({
   style: {
     head: [],
-    border: ['green']
-  }
-});
+    border: ['green'],
+  },
+})
 
-table.push([text]);
+table.push([text])
 
 export default table.toString()
