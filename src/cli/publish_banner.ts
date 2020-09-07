0048be2f693a4b45cd3b6b5654ade3153079ac54
Charles Rudolph <charles.w.rudolph@gmail.com>
2020-09-07 15:55:19 -0700

    more maintainable publish banner

--- src/cli/publish_banner.ts
+++ src/cli/publish_banner.ts
@@ -1,18 +1,27 @@
-const bold = '\u001b[1m'
-const green = '\u001b[32m'
-const cyan = '\u001b[36m'
-const underline = '\u001b[4m'
-const reset = '\u001b[0m'
+import colors from 'colors/safe';
+import Table from 'cli-table3';
 
-export default `${bold}${green}┌──────────────────────────────────────────────────────────────────────────┐${reset}
-${bold}${green}│${reset}${reset} Share your Cucumber Report with your team at ${underline}${bold}${cyan}https://reports.cucumber.io${reset} ${bold}${green}│${reset}
-${bold}${green}│${reset}                                                                          ${bold}${green}│${reset}
-${bold}${green}│${reset} Command line option:    ${cyan}--publish${reset}                                        ${bold}${green}│${reset}
-${bold}${green}│${reset} Environment variable:   ${cyan}CUCUMBER_PUBLISH_ENABLED${reset}=${cyan}true${reset}                    ${bold}${green}│${reset}
-${bold}${green}│${reset}                                                                          ${bold}${green}│${reset}
-${bold}${green}│${reset} More information at ${underline}${bold}${cyan}https://reports.cucumber.io/docs/cucumber-js${reset}         ${bold}${green}│${reset}
-${bold}${green}│${reset}                                                                          ${bold}${green}│${reset}
-${bold}${green}│${reset} To disable this message, add this to your ${bold}./cucumber.js${reset}:                 ${bold}${green}│${reset}
-${bold}${green}│${reset} ${bold}module.exports = { default: '--publish-quiet' }${reset}                          ${bold}${green}│${reset}
-${bold}${green}└──────────────────────────────────────────────────────────────────────────┘${reset}
-`
+const underlineBoldCyan = (x: string) => colors.underline(colors.bold(colors.cyan(x)))
+
+const text = [
+  'Share your Cucumber Report with your team at ' + underlineBoldCyan('https://reports.cucumber.io'),
+  '',
+  'Command line option:    ' + colors.cyan('--publish'),
+  'Environment variable:   ' + colors.cyan('CUCUMBER_PUBLISH_ENABLED') + '=' + colors.cyan('true'),
+  '',
+  'More information at ' + underlineBoldCyan('https://reports.cucumber.io/docs/cucumber-js'),
+  '',
+  'To disable this message, add this to your ' + colors.bold('./cucumber.js') + ':',
+  colors.bold("module.exports = { default: '--publish-quiet' }")
+].join('\n')
+
+const table = new Table({
+  style: {
+    head: [],
+    border: ['green']
+  }
+});
+
+table.push([text]);
+
+export default table.toString()
