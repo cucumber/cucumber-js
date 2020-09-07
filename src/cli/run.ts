5097c2aa2650d4f0f799490a8242cf8b40444a35
Aslak Hellesøy <aslak.hellesoy@smartbear.com>
2020-09-07 15:10:57 +0100

    Add ANSI colours to banner

--- src/cli/run.ts
+++ src/cli/run.ts
@@ -6,17 +6,23 @@ function exitWithError(error: Error): void {
   process.exit(1)
 }
 
-const BANNER = `┌──────────────────────────────────────────────────────────────────────────┐
-│ Share your Cucumber Report with your team at https://reports.cucumber.io │
-│                                                                          │
-│ Command line option:    --publish                                        │
-│ Environment variable:   CUCUMBER_PUBLISH_ENABLED=true                    │
-│                                                                          │
-│ More information at https://reports.cucumber.io/docs/cucumber-js         │
-│                                                                          │
-│ To disable this message, add this to your ./cucumber.js:                 │
-│ module.exports = { default: '--publish-quiet' }                          │
-└──────────────────────────────────────────────────────────────────────────┘
+const bold = '\u001b[1m'
+const green = '\u001b[32m'
+const cyan = '\u001b[36m'
+const underline = '\u001b[4m'
+const reset = '\u001b[0m'
+
+const BANNER = `${bold}${green}┌──────────────────────────────────────────────────────────────────────────┐${reset}
+${bold}${green}│${reset}${reset} Share your Cucumber Report with your team at ${underline}${bold}${cyan}https://reports.cucumber.io${reset} ${bold}${green}│${reset}
+${bold}${green}│${reset}                                                                          ${bold}${green}│${reset}
+${bold}${green}│${reset} Command line option:    ${cyan}--publish${reset}                                        ${bold}${green}│${reset}
+${bold}${green}│${reset} Environment variable:   ${cyan}CUCUMBER_PUBLISH_ENABLED${reset}=${cyan}true${reset}                    ${bold}${green}│${reset}
+${bold}${green}│${reset}                                                                          ${bold}${green}│${reset}
+${bold}${green}│${reset} More information at ${underline}${bold}${cyan}https://reports.cucumber.io/docs/cucumber-js${reset}         ${bold}${green}│${reset}
+${bold}${green}│${reset}                                                                          ${bold}${green}│${reset}
+${bold}${green}│${reset} To disable this message, add this to your ${bold}./cucumber.js${reset}:                 ${bold}${green}│${reset}
+${bold}${green}│${reset} ${bold}module.exports = { default: '--publish-quiet' }${reset}                          ${bold}${green}│${reset}
+${bold}${green}└──────────────────────────────────────────────────────────────────────────┘${reset}
 `
 
 function displayPublishAdvertisementBanner(): void {
