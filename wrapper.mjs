327c8fe9bb3f9864b7f010ee6427e56b91a39c55
davidgoss <david@davidgoss.co>
2021-02-26 01:06:43 +0000

    add mjs wrapper

---
+++ wrapper.mjs
@@ -0,0 +1,6 @@
+import cucumber from './lib/index.js'
+
+export const Given = cucumber.Given
+export const When = cucumber.When
+export const Then = cucumber.Then
+export const BeforeAll = cucumber.BeforeAll
