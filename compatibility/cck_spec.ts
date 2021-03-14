f996c38a3aa40cbac6e1cdbb0c44df742aefca3d
davidgoss <david@davidgoss.co>
2021-03-14 13:22:09 +0000

    make cck fail: remove reordering of testCase messages

--- compatibility/cck_spec.ts
+++ compatibility/cck_spec.ts
@@ -8,7 +8,6 @@ import path from 'path'
 import { PassThrough } from 'stream'
 import { Cli } from '../lib'
 import toString from 'stream-to-string'
-import { doesHaveValue, doesNotHaveValue } from '../src/value_checker'
 import { normalizeMessageOutput } from '../features/support/formatter_output_helpers'
 
 const PROJECT_PATH = path.join(__dirname, '..')
@@ -82,16 +81,5 @@ function normalize(messages: any[]): any[] {
     messages,
     path.join(PROJECT_PATH, 'compatibility')
   )
-  const testCases: any[] = messages.filter((message) =>
-    doesHaveValue(message.testCase)
-  )
-  const everythingElse: any[] = messages.filter((message) =>
-    doesNotHaveValue(message.testCase)
-  )
-  const testRunStarted = everythingElse.findIndex((message) =>
-    doesHaveValue(message.testRunStarted)
-  )
-  // move all `testCase` messages to just after `testRunStarted`
-  everythingElse.splice(testRunStarted + 1, 0, ...testCases)
-  return everythingElse
+  return messages
 }
