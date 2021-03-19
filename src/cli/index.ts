d845e324c4089e66c4a2dc593e47f5b675b340c4
Vincent <vincent.pretre@gmail.com>
2021-03-19 09:32:32 +0100

    Display error banner when Reports server returns status code ≠ 2xx (#1608)
    * Add scenario displaying expected result
    
    * Display error returned by the server instead of stack trace
    Update scenario to ensure cucumber execution fails if the server returned an error
    
    * Make cucumber-js fail when an error is returned by the server
    
    * Rearrange steps like other scenarios
    
    * Disable fail-fast for CI
    
    * Make error first arg in HttpStream to match conventions
    
    * Transform HttpStream from Writable to Transform
    
    * Get one more test passing with new Stream
    
    * Refactoring. Next up: Remove sendRequest recursion
    
    * Refactor HttpStream to avoid recursion
    Get banner displayed as expected
    
    * Simplify HttpStream implementation
    
    * Expect HTTP error in output
    
    * Use progress formatter in CI
    
    Co-authored-by: aurelien-reeves <aurelien.reeves@smartbear.com>
    Co-authored-by: Aslak Hellesøy <aslak.hellesoy@smartbear.com>
    Co-authored-by: vincent.capicotto <vincent.capicotto@hiptest.net>

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -28,6 +28,7 @@ import GherkinStreams from '@cucumber/gherkin/dist/src/stream/GherkinStreams'
 import { ISupportCodeLibrary } from '../support_code_library_builder/types'
 import { IParsedArgvFormatOptions } from './argv_parser'
 import HttpStream from '../formatter/http_stream'
+import { Writable } from 'stream'
 
 const { incrementing, uuid } = IdGenerator
 
@@ -98,14 +99,26 @@ export default class Cli {
               headers.Authorization = `Bearer ${process.env.CUCUMBER_PUBLISH_TOKEN}`
             }
 
-            stream = new HttpStream(outputTo, 'GET', headers, (content) =>
-              console.error(content)
-            )
+            stream = new HttpStream(outputTo, 'GET', headers)
+            const readerStream = new Writable({
+              objectMode: true,
+              write: function (responseBody: string, encoding, writeCallback) {
+                console.error(responseBody)
+                writeCallback()
+              },
+            })
+            stream.pipe(readerStream)
           } else {
             const fd = await fs.open(path.resolve(this.cwd, outputTo), 'w')
             stream = fs.createWriteStream(null, { fd })
           }
         }
+
+        stream.on('error', (error) => {
+          console.error(error.message)
+          process.exit(1)
+        })
+
         const typeOptions = {
           cwd: this.cwd,
           eventBroadcaster,
