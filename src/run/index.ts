73f138919478e1b21ac0c8d65480015c3b62e1e8
David Goss <david@davidgoss.co>
2021-07-08 14:15:34 +0100

    keep going

--- src/run/index.ts
+++ src/run/index.ts
@@ -1,13 +1,14 @@
 import { IParsedArgvFormatOptions } from '../cli/argv_parser'
 import { ISupportCodeLibrary } from '../support_code_library_builder/types'
 
-export interface IRunParameters {
-  gherkin: {
-    defaultDialect: string
-  }
-  pickles: {
+interface IRunCucumberOptions {
+  cwd: string
+  features: {
+    defaultDialect?: string
     paths: string[]
-    names?: string[]
+  }
+  filters: {
+    name?: string[]
     tagExpression?: string
   }
   support:
@@ -43,7 +44,43 @@ export interface IRunResult {
 }
 
 export async function runCucumber(
-  options: IRunParameters
+  options: IRunCucumberOptions
 ): Promise<IRunResult> {
   return null
 }
+
+const result = await runCucumber({
+  cwd: process.cwd(),
+  features: {
+    paths: ['features/**/*.feature'],
+  },
+  filters: {
+    name: ['Acme'],
+    tagExpression: '@interesting',
+  },
+  support: {
+    transpileWith: ['ts-node'],
+    paths: ['features/support/**/*.ts'],
+  },
+  runtime: {
+    failFast: true,
+    retry: {
+      count: 1,
+      tagExpression: '@flaky',
+    },
+    strict: true,
+    worldParameters: {
+      foo: 'bar',
+    },
+  },
+  formats: {
+    stdout: '@cucumber/pretty-formatter',
+    files: {
+      'report.html': 'html',
+      'TEST-cucumber.xml': 'junit',
+    },
+    options: {
+      printAttachments: false,
+    },
+  },
+})
