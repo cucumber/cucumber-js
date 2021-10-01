2d2c89cff8fc8ff3c1d03de9a578d9cd7aa8a304
unknown <tomerpacific@gmail.com>
2021-10-01 11:59:51 +0300

    feature/list-formatters-in-help-command creating Formatters class to hold different formatters and extracting them from the builder class

---
+++ src/formatter/helpers/formatters.ts
@@ -0,0 +1,30 @@
+import Formatter from '../.'
+import JsonFormatter from '../json_formatter'
+import MessageFormatter from '../message_formatter'
+import ProgressBarFormatter from '../progress_bar_formatter'
+import ProgressFormatter from '../progress_formatter'
+import RerunFormatter from '../rerun_formatter'
+import SnippetsFormatter from '../snippets_formatter'
+import SummaryFormatter from '../summary_formatter'
+import UsageFormatter from '../usage_formatter'
+import UsageJsonFormatter from '../usage_json_formatter'
+import HtmlFormatter from '../html_formatter'
+
+const Formatters = {
+  getFormatters(): Record<string, typeof Formatter> {
+    return {
+      json: JsonFormatter,
+      message: MessageFormatter,
+      html: HtmlFormatter,
+      progress: ProgressFormatter,
+      'progress-bar': ProgressBarFormatter,
+      rerun: RerunFormatter,
+      snippets: SnippetsFormatter,
+      summary: SummaryFormatter,
+      usage: UsageFormatter,
+      'usage-json': UsageJsonFormatter,
+    }
+  },
+}
+
+export default Formatters
