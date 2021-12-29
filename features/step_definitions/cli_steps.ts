385c26237ed2eea04b1ab808ccbc1edc23df3f06
Karla Aparecida Justen <justen.karla@gmail.com>
2021-12-29 04:40:48 -0300

    Replace 1 instance of regex with cucumber expression (#1872)
    * Replace regex with cucumber expression.
    
    We decided to split the step definition into two. So that the patterns used be simpler.
    
    Co-authored-by: Blaise Pabon <blaise@gmail.com>
    Co-authored-by: Matt Wynne <matt@cucumber.io>
    Co-authored-by: Dane Parchment Jr <dparchmentjr@gmail.com>
    
    * Fix linting issues
    
    Co-authored-by: Blaise Pabon <blaise@gmail.com>
    Co-authored-by: Matt Wynne <matt@cucumber.io>
    Co-authored-by: Dane Parchment Jr <dparchmentjr@gmail.com>
    Co-authored-by: aurelien-reeves <aurelien.reeves@smartbear.com>

--- features/step_definitions/cli_steps.ts
+++ features/step_definitions/cli_steps.ts
@@ -15,8 +15,12 @@ When('my env includes {string}', function (this: World, envString: string) {
   this.sharedEnv = this.parseEnvString(envString)
 })
 
+When('I run cucumber-js', { timeout: 10000 }, async function (this: World) {
+  return await this.run(this.localExecutablePath, [])
+})
+
 When(
-  /^I run cucumber-js(?: with `(|.+)`)?$/,
+  'I run cucumber-js with `{}`',
   { timeout: 10000 },
   async function (this: World, args: string) {
     const renderedArgs = Mustache.render(valueOrDefault(args, ''), this)
