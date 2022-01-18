c99ad50b4c2486c54a2ad7abb7b05bbe8c65b030
Matt Wynne <matt@cucumber.io>
2022-01-18 11:39:15 -0800

    Replace some uses of `any` type (#1892)
    * Replace use of `any` type with `messages.Envelope`
    
    Part of #1648
    
    Co-authored-by: Kate Dames <kate.funficient@gmail.com>
    Co-authored-by: Emmanuel Ola <54866720+eoola@users.noreply.github.com>
    
    * Replace use of `any` with a custom World in CCK example
    
    Part of #1648
    
    Co-authored-by: Emmanuel Ola <54866720+eoola@users.noreply.github.com>
    Co-authored-by: Blaise Pabon <blaise@gmail.com>
    Co-authored-by: Kate Dames <kate.funficient@gmail.com>
    
    * Replace another use of `any` with a custom type
    
    Co-authored-by: Blaise Pabon <blaise@gmail.com>
    Co-authored-by: Kate Dames <kate.funficient@gmail.com>
    
    * Replace another use of `any` type
    
    Co-authored-by: Blaise Pabon <blaise@gmail.com>
    Co-authored-by: Kate Dames <kate.funficient@gmail.com>
    
    Co-authored-by: Kate Dames <kate.funficient@gmail.com>
    Co-authored-by: Emmanuel Ola <54866720+eoola@users.noreply.github.com>
    Co-authored-by: Blaise Pabon <blaise@gmail.com>

--- compatibility/features/examples-tables/examples-tables.ts
+++ compatibility/features/examples-tables/examples-tables.ts
@@ -1,17 +1,24 @@
 import assert from 'assert'
 import { Given, When, Then } from '../../../src'
 
-Given('there are {int} cucumbers', function (this: any, initialCount: number) {
-  this.count = initialCount
-})
+type World = {
+  count: number
+}
 
-When('I eat {int} cucumbers', function (this: any, eatCount: number) {
+Given(
+  'there are {int} cucumbers',
+  function (this: World, initialCount: number) {
+    this.count = initialCount
+  }
+)
+
+When('I eat {int} cucumbers', function (this: World, eatCount: number) {
   this.count -= eatCount
 })
 
 Then(
   'I should have {int} cucumbers',
-  function (this: any, expectedCount: number) {
+  function (this: World, expectedCount: number) {
     assert.strictEqual(this.count, expectedCount)
   }
 )
