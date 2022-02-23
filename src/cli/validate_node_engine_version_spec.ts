cc570fd5056577e39a52cc6294c7c9f1945bb07e
David Goss <david@davidgoss.co>
2022-02-23 18:15:14 +0000

    fix bad merge

--- src/cli/validate_node_engine_version_spec.ts
+++ src/cli/validate_node_engine_version_spec.ts
@@ -21,6 +21,9 @@ describe(validateNodeEngineVersion.name, () => {
   })
 
   it('calls the onError callback when the version is between our supported versions', () => {
+    // Arrange
+    const errorSpy = sinon.spy()
+    
     validateNodeEngineVersion('v13.1.2', errorSpy, () => ({
       engines: {
         node: '12 || 14 || 16 || 17',
