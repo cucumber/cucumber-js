c91efda8a69ac8a7b941bf69520bb5151d13cd52
David Goss <david@davidgoss.co>
2022-03-04 16:40:00 +0000

    expand this test a bit

--- src/configuration/merge_configurations_spec.ts
+++ src/configuration/merge_configurations_spec.ts
@@ -2,10 +2,17 @@ import { expect } from 'chai'
 import { mergeConfigurations } from './merge_configurations'
 
 describe('mergeConfigurations', () => {
-  it('should merge arrays correctly', () => {
+  it('should merge two arrays correctly', () => {
     const result = mergeConfigurations({ paths: ['a'] }, { paths: ['b'] })
     expect(result).to.deep.eq({
       paths: ['a', 'b'],
+    })
+  })
+
+  it('should merge one array and one nullish correctly', () => {
+    const result = mergeConfigurations({ paths: ['a'] }, { paths: undefined })
+    expect(result).to.deep.eq({
+      paths: ['a'],
     })
   })
 })
