af54d051989c39a64b776998d5ce4b32be0f2d53
David Goss <david@davidgoss.co>
2022-03-09 14:45:51 +0000

    update message on retry config validation

--- src/configuration/validate_configuration.ts
+++ src/configuration/validate_configuration.ts
@@ -3,7 +3,7 @@ import { IConfiguration } from './types'
 export function validateConfiguration(configuration: IConfiguration): void {
   if (configuration.retryTagFilter && !configuration.retry) {
     throw new Error(
-      'a positive --retry count must be specified when setting --retry-tag-filter'
+      'a positive `retry` count must be specified when setting `retryTagFilter`'
     )
   }
 }
