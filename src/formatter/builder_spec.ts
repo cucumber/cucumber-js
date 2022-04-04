bb9a194221136732a87bc7284b28bfe6982e3d97
David Goss <david@davidgoss.co>
2022-04-04 20:56:31 +0100

    try require first when loadinh formatters

--- src/formatter/builder_spec.ts
+++ src/formatter/builder_spec.ts
@@ -4,46 +4,32 @@ import { pathToFileURL } from 'url'
 import path from 'path'
 
 describe('custom class loading', () => {
-  it('should handle a file:// url', async () => {
-    const fileUrl = pathToFileURL(
-      path.resolve(__dirname, './fixtures/module_dot_exports.cjs')
-    ).toString()
-    const CustomClass = await FormatterBuilder.loadCustomClass(
-      'formatter',
-      fileUrl,
-      __dirname
-    )
+  ;['esm.mjs', 'exports_dot_default.cjs', 'module_dot_exports.cjs'].forEach(
+    (filename) => {
+      describe(filename, () => {
+        it('should handle a relative path', async () => {
+          const CustomClass = await FormatterBuilder.loadCustomClass(
+            'formatter',
+            `./fixtures/${filename}`,
+            __dirname
+          )
 
-    expect(typeof CustomClass).to.eq('function')
-  })
+          expect(typeof CustomClass).to.eq('function')
+        })
 
-  it('should handle cjs module.exports', async () => {
-    const CustomClass = await FormatterBuilder.loadCustomClass(
-      'formatter',
-      './fixtures/module_dot_exports.cjs',
-      __dirname
-    )
+        it('should handle a file:// url', async () => {
+          const fileUrl = pathToFileURL(
+            path.resolve(__dirname, `./fixtures/${filename}`)
+          ).toString()
+          const CustomClass = await FormatterBuilder.loadCustomClass(
+            'formatter',
+            fileUrl,
+            __dirname
+          )
 
-    expect(typeof CustomClass).to.eq('function')
-  })
-
-  it('should handle cjs exports.default', async () => {
-    const CustomClass = await FormatterBuilder.loadCustomClass(
-      'formatter',
-      './fixtures/exports_dot_default.cjs',
-      __dirname
-    )
-
-    expect(typeof CustomClass).to.eq('function')
-  })
-
-  it('should handle esm default export', async () => {
-    const CustomClass = await FormatterBuilder.loadCustomClass(
-      'formatter',
-      './fixtures/esm.mjs',
-      __dirname
-    )
-
-    expect(typeof CustomClass).to.eq('function')
-  })
+          expect(typeof CustomClass).to.eq('function')
+        })
+      })
+    }
+  )
 })
