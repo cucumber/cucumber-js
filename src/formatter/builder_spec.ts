558d5176f723252254f0e44a9a4655d3f7446422
David Goss <david@davidgoss.co>
2022-04-04 20:58:28 +0100

    prove that transpiled ones work too

--- src/formatter/builder_spec.ts
+++ src/formatter/builder_spec.ts
@@ -4,32 +4,36 @@ import { pathToFileURL } from 'url'
 import path from 'path'
 
 describe('custom class loading', () => {
-  ;['esm.mjs', 'exports_dot_default.cjs', 'module_dot_exports.cjs'].forEach(
-    (filename) => {
-      describe(filename, () => {
-        it('should handle a relative path', async () => {
-          const CustomClass = await FormatterBuilder.loadCustomClass(
-            'formatter',
-            `./fixtures/${filename}`,
-            __dirname
-          )
+  const varieties = [
+    'esm.mjs',
+    'exports_dot_default.cjs',
+    'module_dot_exports.cjs',
+    'typescript.ts',
+  ]
+  varieties.forEach((filename) => {
+    describe(filename, () => {
+      it('should handle a relative path', async () => {
+        const CustomClass = await FormatterBuilder.loadCustomClass(
+          'formatter',
+          `./fixtures/${filename}`,
+          __dirname
+        )
 
-          expect(typeof CustomClass).to.eq('function')
-        })
-
-        it('should handle a file:// url', async () => {
-          const fileUrl = pathToFileURL(
-            path.resolve(__dirname, `./fixtures/${filename}`)
-          ).toString()
-          const CustomClass = await FormatterBuilder.loadCustomClass(
-            'formatter',
-            fileUrl,
-            __dirname
-          )
-
-          expect(typeof CustomClass).to.eq('function')
-        })
+        expect(typeof CustomClass).to.eq('function')
       })
-    }
-  )
+
+      it('should handle a file:// url', async () => {
+        const fileUrl = pathToFileURL(
+          path.resolve(__dirname, `./fixtures/${filename}`)
+        ).toString()
+        const CustomClass = await FormatterBuilder.loadCustomClass(
+          'formatter',
+          fileUrl,
+          __dirname
+        )
+
+        expect(typeof CustomClass).to.eq('function')
+      })
+    })
+  })
 })
