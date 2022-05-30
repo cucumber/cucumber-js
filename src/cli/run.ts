037b829e52d5b2461ed0d6adff0a8e9eefdd55b8
David Goss <david@davidgoss.co>
2022-05-30 08:22:16 +0100

    restore old run.ts

--- src/cli/run.ts
+++ src/cli/run.ts
@@ -1,52 +1,35 @@
+/* eslint-disable no-console */
+/* This is one rare place where we're fine to use process/console directly,
+ * but other code abstracts those to remain composable and testable. */
 import Cli, { ICliRunResult } from './'
 import VError from 'verror'
 import publishBanner from './publish_banner'
 import { validateNodeEngineVersion } from './validate_node_engine_version'
-import { supportsColor } from 'supports-color'
-import stripAnsiStream from 'strip-ansi-stream'
-import { Console } from 'console'
 
-function logErrorMessageAndExit(logger: Console, message: string): void {
-  logger.error(message)
+function logErrorMessageAndExit(message: string): void {
+  console.error(message)
   process.exit(1)
 }
 
-function displayPublishAdvertisementBanner(logger: Console): void {
-  logger.error(publishBanner)
-}
-
-/*
-If process.stderr doesn't support ANSI characters, pipe it through a stream that
-strips them out. This is because some output we send to stderr uses ANSI
-without checking for support, and we can't control it (for now).
- */
-function sanitiseStdErr() {
-  if (supportsColor(process.stderr)) {
-    return process.stderr
-  }
-  const sanitised = stripAnsiStream()
-  sanitised.pipe(process.stderr, { end: false })
-  return sanitised
+function displayPublishAdvertisementBanner(): void {
+  console.error(publishBanner)
 }
 
 export default async function run(): Promise<void> {
-  const stderr = sanitiseStdErr()
-  const logger = new Console(stderr)
-
   validateNodeEngineVersion(
     process.version,
     (error) => {
-      logger.error(error)
+      console.error(error)
       process.exit(1)
     },
-    logger.warn
+    console.warn
   )
 
   const cli = new Cli({
     argv: process.argv,
     cwd: process.cwd(),
     stdout: process.stdout,
-    stderr,
+    stderr: process.stderr,
     env: process.env,
   })
 
@@ -54,11 +37,11 @@ export default async function run(): Promise<void> {
   try {
     result = await cli.run()
   } catch (error) {
-    logErrorMessageAndExit(logger, VError.fullStack(error))
+    logErrorMessageAndExit(VError.fullStack(error))
   }
 
   if (result.shouldAdvertisePublish) {
-    displayPublishAdvertisementBanner(logger)
+    displayPublishAdvertisementBanner()
   }
 
   const exitCode = result.success ? 0 : 1
