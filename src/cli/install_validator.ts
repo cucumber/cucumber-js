936f04c35fbfc5721f28c3b32be634cca34c51fd
David Goss <david@davidgoss.co>
2022-07-17 10:36:44 +0100

    use is-installed-globally, emit warning

--- src/cli/install_validator.ts
+++ src/cli/install_validator.ts
@@ -1,39 +1,13 @@
-import fs from 'mz/fs'
-import path from 'path'
-import resolve from 'resolve'
-import { promisify } from 'util'
+import isInstalledGlobally from 'is-installed-globally'
 
-export async function validateInstall(cwd: string): Promise<void> {
-  const projectPath = path.join(__dirname, '..', '..')
-  if (projectPath === cwd) {
-    return // cucumber testing itself
-  }
-  const currentCucumberPath = require.resolve(projectPath)
-  let localCucumberPath: string
-  try {
-    localCucumberPath = await promisify<string, resolve.Opts, string>(resolve)(
-      '@cucumber/cucumber',
-      {
-        basedir: cwd,
-      }
-    )
-  } catch (e) {
-    throw new Error(
-      '`@cucumber/cucumber` module not resolvable. Must be locally installed.'
-    )
-  }
-  localCucumberPath = await fs.realpath(localCucumberPath)
-  if (localCucumberPath !== currentCucumberPath) {
-    throw new Error(
+export async function validateInstall(): Promise<void> {
+  if (isInstalledGlobally)
+    console.warn(
       `
       You appear to be executing an install of cucumber (most likely a global install)
       that is different from your local install (the one required in your support files).
       For cucumber to work, you need to execute the same install that is required in your support files.
       Please execute the locally installed version to run your tests.
-
-      Executed Path: ${currentCucumberPath}
-      Local Path:    ${localCucumberPath}
       `
     )
-  }
 }
