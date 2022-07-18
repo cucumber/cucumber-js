4cf66978a94f2a1c34f2eeabd8c33d0d9bfee537
David Goss <david@davidgoss.co>
2022-07-18 08:52:59 +0100

    tweak global message

--- src/cli/install_validator.ts
+++ src/cli/install_validator.ts
@@ -5,10 +5,9 @@ export async function validateInstall(): Promise<void> {
   if (isInstalledGlobally)
     console.warn(
       `
-      You appear to be executing an install of cucumber (most likely a global install)
-      that is different from your local install (the one required in your support files).
-      For cucumber to work, you need to execute the same install that is required in your support files.
-      Please execute the locally installed version to run your tests.
+      It looks like you're running Cucumber from a global installation.
+      This won't work; you need to have Cucumber installed as a local dependency in your project.
+      See https://github.com/cucumber/cucumber-js/blob/main/docs/faq.md
       `
     )
 }
