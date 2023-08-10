dbe49acf68269c4c2154bd8cd56b15fcba68bfe5
Yusuke Noda <yusuke.noda.fq@hitachi.com>
2023-08-10 14:21:55 +0900

    reimplement OptionSplitter
    Reimplement OptionSplitter and fix the issue discussed in #2178

--- src/configuration/option_splitter.ts
+++ src/configuration/option_splitter.ts
@@ -1,25 +1,54 @@
 export const OptionSplitter = {
   split(option: string): string[] {
-    option = option.replace(/"/g, '')
+    let result: string[];
+    let match1, match2;
 
-    const parts = option.split(/((?:file){0}):(?!\\|\/\/)/)
-
-    const result = parts.reduce((memo: string[], part: string, i: number) => {
-      if (partNeedsRecombined(i)) {
-        memo.push(parts.slice(i, i + 2).join(''))
+    // "foo":"bar" or "foo":bar
+    if (match1 = option.match(/^"([^"]*)":(.*)$/)) {
+      // "foo":"bar"
+      if (match2 = match1[2].match(/^"([^"]*)"$/)) {
+        result = [match1[1], match2[1]];
       }
-
-      return memo
-    }, [])
-
-    if (result.length === 1) {
-      result.push('')
+      // "foo":bar
+      else {
+        result = [match1[1], match1[2]];
+      }
+    }
+    // foo:"bar"
+    else if (match1 = option.match(/^(.*):"([^"]*)"$/)) {
+      result = [match1[1], match1[2]];
+    }
+    // "foo"
+    else if (match1 = option.match(/^"([^"]*)"$/)) {
+      result = [match1[1], ''];
+    }
+    // file://foo or file:///foo or file://C:/foo or file://C:\foo or file:///C:/foo or file:///C:\foo
+    else if (match1 = option.match(/^(file:\/{2,3}(?:[a-zA-Z]:[\/\\])?)(.*)$/)) {
+      // file://foo:bar
+      if (match2 = match1[2].match(/^([^:]*):(.*)$/)) {
+        result = [match1[1] + match2[1], match2[2]];
+      } else {
+        result = [option, ''];
+      }
+    }
+    // C:\foo or C:/foo
+    else if (match1 = option.match(/^([a-zA-Z]:[\/\\])(.*)$/)) {
+      // C:\foo:bar or C:/foo:bar
+      if (match2 = match1[2].match(/^([^:]*):(.*)$/)) {
+        result = [match1[1] + match2[1], match2[2]];
+      } else {
+        result = [option, ''];
+      }
+    }
+    // foo:bar
+    else if (match1 = option.match(/^([^:]*):(.*)$/)) {
+      result = [match1[1], match1[2]];
+    }
+    // foo
+    else {
+      result = [option, ''];
     }
 
-    return result
+    return result;
   },
-}
-
-function partNeedsRecombined(i: number): boolean {
-  return i % 2 === 0
 }
--- src/configuration/option_splitter_spec.ts
+++ src/configuration/option_splitter_spec.ts
@@ -15,17 +15,22 @@ describe('OptionSplitter', () => {
       output: ['../custom/formatter', '../formatter/output.txt'],
     },
     {
-      description: 'splits absolute unix paths',
+      description: 'splits relative windows paths',
+      input: '..\\custom\\formatter:..\\formatter\\output.txt',
+      output: ['..\\custom\\formatter', '..\\formatter\\output.txt'],
+    },
+    {
+      description: 'splits file URLs for absolute unix path',
       input: 'file:///custom/formatter:file:///formatter/output.txt',
       output: ['file:///custom/formatter', 'file:///formatter/output.txt'],
     },
     {
-      description: 'splits paths with quotes around them',
-      input: '/custom/formatter:"/formatter directory/output.txt"',
-      output: ['/custom/formatter', '/formatter directory/output.txt'],
+      description: 'splits file URLs for UNC path',
+      input: 'file://hostname/custom/formatter:file://hostname/formatter/output.txt',
+      output: ['file://hostname/custom/formatter', 'file://hostname/formatter/output.txt'],
     },
     {
-      description: 'splits absolute windows paths',
+      description: 'splits file URLs for absolute windows path',
       input: 'file://C:\\custom\\formatter:file://C:\\formatter\\output.txt',
       output: [
         'file://C:\\custom\\formatter',
@@ -33,10 +38,146 @@ describe('OptionSplitter', () => {
       ],
     },
     {
+      description: 'splits file URLs for absolute windows path with "/" as directory separator',
+      input: 'file:///C:/custom/formatter:file:///C:/formatter/output.txt',
+      output: [
+        'file:///C:/custom/formatter',
+        'file:///C:/formatter/output.txt',
+      ],
+    },
+    {
+      description: 'splits valid file URLs for absolute windows path',
+      input: 'file:///C:\\custom\\formatter:file:///C:\\formatter\\output.txt',
+      output: [
+        'file:///C:\\custom\\formatter',
+        'file:///C:\\formatter\\output.txt',
+      ],
+    },
+    {
+      description: 'splits valid file URLs for absolute windows path with "/" as directory separator',
+      input: 'file:///C:/custom/formatter:file:///C:/formatter/output.txt',
+      output: [
+        'file:///C:/custom/formatter',
+        'file:///C:/formatter/output.txt',
+      ],
+    },
+    {
+      description: 'splits absolute unix paths',
+      input: '/custom/formatter:/formatter/output.txt',
+      output: [
+        '/custom/formatter',
+        '/formatter/output.txt',
+      ],
+    },
+    {
+      description: 'splits absolute windows paths',
+      input: 'C:\\custom\\formatter:C:\\formatter\\output.txt',
+      output: [
+        'C:\\custom\\formatter',
+        'C:\\formatter\\output.txt',
+      ],
+    },
+    {
+      description: 'splits absolute windows paths with "/" as directory separator',
+      input: 'C:/custom/formatter:C:/formatter/output.txt',
+      output: [
+        'C:/custom/formatter',
+        'C:/formatter/output.txt',
+      ],
+    },
+    {
+      description: 'splits UNC paths',
+      input: '\\\\hostname\\custom\\formatter:\\\\hostname\\formatter\\output.txt',
+      output: [
+        '\\\\hostname\\custom\\formatter',
+        '\\\\hostname\\formatter\\output.txt',
+      ],
+    },
+    {
+      description: 'splits UNC paths with "/" as directory separator',
+      input: '//hostname/custom/formatter://hostname/formatter/output.txt',
+      output: [
+        '//hostname/custom/formatter',
+        '//hostname/formatter/output.txt',
+      ],
+    },
+    {
+      description: 'splits paths with quotes around them',
+      input: '/custom/formatter:"/formatter directory/output.txt"',
+      output: ['/custom/formatter', '/formatter directory/output.txt'],
+    },
+    {
       description:
-        'does not split a single absolute windows paths, adds empty string',
+        'does not split a single file URL for absolute unix path, adds empty string',
+      input: 'file:///custom/formatter',
+      output: ['file:///custom/formatter', ''],
+    },
+    {
+      description:
+        'does not split a single file URL for UNC path, adds empty string',
+      input: 'file://hostname/custom/formatter',
+      output: ['file://hostname/custom/formatter', ''],
+    },
+    {
+      description:
+        'does not split a single file URL for absolute windows path, adds empty string',
       input: 'file://C:\\custom\\formatter',
       output: ['file://C:\\custom\\formatter', ''],
+    },
+    {
+      description:
+        'does not split a single file URL for absolute windows path with "/" as directory separator, adds empty string',
+      input: 'file://C:/custom/formatter',
+      output: ['file://C:/custom/formatter', ''],
+    },
+    {
+      description:
+        'does not split a valid single file URL for absolute windows path, adds empty string',
+      input: 'file:///C:\\custom\\formatter',
+      output: ['file:///C:\\custom\\formatter', ''],
+    },
+    {
+      description:
+        'does not split a valid single file URL for absolute windows path with "/" as directory separator, adds empty string',
+      input: 'file:///C:/custom/formatter',
+      output: ['file:///C:/custom/formatter', ''],
+    },
+    {
+      description:
+        'does not split a single absolute windows path, adds empty string',
+      input: 'C:\\custom\\formatter',
+      output: ['C:\\custom\\formatter', ''],
+    },
+    {
+      description:
+        'does not split a single absolute windows path with "/" as directory separator, adds empty string',
+      input: 'C:/custom/formatter',
+      output: ['C:/custom/formatter', ''],
+    },
+    {
+      description: 'does not split quoted values: case 1',
+      input: '"foo:bar":"baz:qux"',
+      output: ['foo:bar', 'baz:qux'],
+    },
+    {
+      description: 'does not split quoted values: case 2',
+      input: '"foo:bar":baz:qux',
+      output: ['foo:bar', 'baz:qux'],
+    },
+    {
+      description: 'does not split quoted values: case 3',
+      input: 'foo:bar:"baz:qux"',
+      output: ['foo:bar', 'baz:qux'],
+    },
+    {
+      description: 'does not split quoted values: case 4',
+      input: '"foo:bar:baz:qux"',
+      output: ['foo:bar:baz:qux', ''],
+    },
+    {
+      description: 'splits string contains multiple ":"',
+      input: 'foo:bar:baz:qux',
+      output: ['foo', 'bar:baz:qux'],
     },
   ]
 
