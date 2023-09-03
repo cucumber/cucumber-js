0a9fad0eaf5dcdcb7b14b71b2c963f439d5af75c
David Goss <david@davidgoss.co>
2023-09-03 12:30:44 +0100

    better formats config (#2318)

--- src/api/convert_configuration_spec.ts
+++ src/api/convert_configuration_spec.ts
@@ -39,7 +39,7 @@ describe('convertConfiguration', () => {
     })
   })
 
-  it('should map multiple formatters', async () => {
+  it('should map multiple formatters with string and array notations', async () => {
     const result = await convertConfiguration(
       {
         ...DEFAULT_CONFIGURATION,
@@ -47,7 +47,7 @@ describe('convertConfiguration', () => {
           'summary',
           'message',
           'json:./report.json',
-          'html:./report.html',
+          ['html', './report.html'],
         ],
       },
       {}
