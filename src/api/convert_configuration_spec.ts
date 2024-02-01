e594757b7c0a55cf961fc877a1bc978a4acea7b2
guy arieli <guy.arieli@blinq.io>
2024-02-01 15:48:39 +0200

    update tests

--- src/api/convert_configuration_spec.ts
+++ src/api/convert_configuration_spec.ts
@@ -14,6 +14,7 @@ describe('convertConfiguration', () => {
         publish: false,
         stdout: 'progress',
       },
+      runName: '',
       runtime: {
         dryRun: false,
         failFast: false,
