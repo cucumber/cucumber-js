6a2e7bdcc9e6c77504040abf0982b94471589fec
David Goss <david@davidgoss.co>
2024-06-22 09:41:10 +0100

    implement externalisation

--- src/formatter/builtin/html.ts
+++ src/formatter/builtin/html.ts
@@ -1,7 +1,16 @@
 import { promisify } from 'node:util'
 import { finished } from 'node:stream'
+import { writeFile } from 'node:fs'
+import path from 'node:path'
 import CucumberHtmlStream from '@cucumber/html-formatter'
 import resolvePkg from 'resolve-pkg'
+import mimeTypes from 'mime'
+import {
+  Attachment,
+  AttachmentContentEncoding,
+  Envelope,
+  IdGenerator,
+} from '@cucumber/messages'
 import { FormatterPlugin } from '../../plugin'
 
 interface Options {
@@ -10,22 +19,78 @@ interface Options {
 
 export default {
   type: 'formatter',
-  formatter({ on, logger, write, directory }) {
-    logger.warn('Directory for HTML formatter: ' + directory)
+  formatter({ on, options, write, directory }) {
+    const newId = IdGenerator.uuid()
     const htmlStream = new CucumberHtmlStream(
       resolvePkg('@cucumber/html-formatter', { cwd: __dirname }) +
         '/dist/main.css',
       resolvePkg('@cucumber/html-formatter', { cwd: __dirname }) +
         '/dist/main.js'
     )
-    on('message', (message) => htmlStream.write(message))
+    const writeOperations: Promise<void>[] = []
+    on('message', (message) => {
+      if (message.attachment && options.externalAttachments) {
+        const { attachment, writeOperation } = externaliseAttachment(
+          newId,
+          message.attachment,
+          directory
+        )
+        htmlStream.write({
+          ...message,
+          attachment,
+        } satisfies Envelope)
+        if (writeOperation) {
+          writeOperations.push(writeOperation)
+        }
+      } else {
+        htmlStream.write(message)
+      }
+    })
     htmlStream.on('data', (chunk) => write(chunk))
 
     return async () => {
       htmlStream.end()
       await promisify(finished)(htmlStream)
+      await Promise.all(writeOperations)
     }
   },
   documentation: 'Outputs a HTML report',
   optionsKey: 'html',
 } satisfies FormatterPlugin<Options>
+
+const encodingsMap = {
+  IDENTITY: 'utf-8',
+  BASE64: 'base64',
+} as const
+
+function externaliseAttachment(
+  newId: () => string,
+  original: Attachment,
+  directory?: string
+) {
+  if (!directory) {
+    return { attachment: original }
+  }
+  if (original.mediaType === 'text/x.cucumber.log+plain') {
+    return { attachment: original }
+  }
+  let filename = `attachment-${newId()}`
+  const extension = mimeTypes.getExtension(original.mediaType)
+  if (extension) {
+    filename += `.${extension}`
+  }
+  const writeOperation = promisify(writeFile)(
+    path.join(directory, filename),
+    Buffer.from(original.body, encodingsMap[original.contentEncoding])
+  )
+  const updated: Attachment = {
+    ...original,
+    contentEncoding: AttachmentContentEncoding.IDENTITY,
+    body: '',
+    url: `./${filename}`,
+  }
+  return {
+    attachment: updated,
+    writeOperation,
+  }
+}
