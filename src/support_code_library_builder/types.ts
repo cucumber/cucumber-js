471a89d0f97f78c5dd3ee3061e163952d91db6e1
David Goss <david@davidgoss.co>
2025-06-11 00:50:45 +0100

    Add error to After hook parameter

--- src/support_code_library_builder/types.ts
+++ src/support_code_library_builder/types.ts
@@ -16,6 +16,7 @@ export interface ITestCaseHookParameter {
   gherkinDocument: messages.GherkinDocument
   pickle: messages.Pickle
   result?: messages.TestStepResult
+  error?: any
   willBeRetried?: boolean
   testCaseStartedId: string
 }
@@ -25,6 +26,7 @@ export interface ITestStepHookParameter {
   pickle: messages.Pickle
   pickleStep: messages.PickleStep
   result: messages.TestStepResult
+  error?: any
   testCaseStartedId: string
   testStepId: string
 }
