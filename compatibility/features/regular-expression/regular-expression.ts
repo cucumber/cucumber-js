a494536de64a47ca9dff263bf71abe820c3d126b
David Goss <david@davidgoss.co>
2025-09-06 10:55:21 +0100

    add regexp fixture

---
+++ compatibility/features/regular-expression/regular-expression.ts
@@ -0,0 +1,6 @@
+import { Given } from '../../../src'
+
+Given(/^a (.*?)(?: and a (.*?))?(?: and a (.*?))?$/,
+  function (vegtable1: string, vegtable2: string, vegtable3: string) {
+    // no-op
+  })
